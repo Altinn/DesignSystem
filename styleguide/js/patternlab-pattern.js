28e481fed86d0f8b76f5fc12e83d89424e8838aa
MikaeI <mikael@persille.no>
2016-08-18 10:42:58 +0200

    Updates.

--- styleguide/js/patternlab-pattern.js
+++ styleguide/js/patternlab-pattern.js
@@ -84,10 +84,12 @@ function receiveIframeMessage(event) {
       path = window.location.protocol+"//"+window.location.host+window.location.pathname.replace("styleguide\/html\/styleguide.html","")+data.path+'?'+Date.now();
       var count = (path.match(/\.html/g) || []).length;
       if (count < 2) {
+        console.log('siste1', path)
         window.location.replace(path);
       } else if (data.path !== 'styleguide/html/styleguide.html') {
         var re2 = /(patterns|snapshots)\/(.*)$/;
         path = window.location.protocol+"//"+window.location.host+window.location.pathname.replace(re2,'')+data.path+'?'+Date.now();
+        console.log('siste2', path)
         window.location.replace(path);
       }
 
