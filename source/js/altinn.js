23d11513a7f9c4918a2b87abe0429a71b9e3bc9f
MikaeI <mikael@persille.no>
2016-09-02 15:32:09 +0200

    Misc.

--- source/js/altinn.js
+++ source/js/altinn.js
@@ -1,4 +1,4 @@
-/* globals anchors, jQuery, $, localStorage */
+/* globals anchors, $, localStorage */
 window.altinnInit = function () {
   var bpLarge = 992; var options
   // Toggle elements with the same parent + add 'open'-class
@@ -33,8 +33,6 @@ window.altinnInit = function () {
     var toggleStuff = function () {
       $('body', $('iframe').contents()[0]).toggleClass('business')
       $('body', $('iframe').contents()[0]).toggleClass('private-person')
-      $('html', $('iframe').contents()[0]).toggleClass('business')
-      $('html', $('iframe').contents()[0]).toggleClass('private-person')
       localStorage
         .setItem('theme', $('body', $('iframe').contents()[0]).attr('class'))
       $('body', '.ap-profile').attr('class', '')
@@ -61,198 +59,142 @@ window.altinnInit = function () {
       var actionRow = $(this).attr('data-target')
       if (!$(this).hasClass('collapsed')) {
         $(this).addClass('collapsed')
-        if($(this).parent().is('td')) {
-            $(actionRow).prev().removeClass('open')
-            $(actionRow).css('display', 'none')
+        if ($(this).parent().is('td')) {
+          $(actionRow).prev().removeClass('open')
+          $(actionRow).css('display', 'none')
         }
       } else {
         $('.a-collapseContent').removeClass('in')
         $('.a-collapseFilter').addClass('collapsed')
         $(this).removeClass('collapsed')
-        if($(this).parent().is('td')) {
-            $(actionRow).css('display', 'table-row')
-            $(actionRow).prev().addClass('open')
+        if ($(this).parent().is('td')) {
+          $(actionRow).css('display', 'table-row')
+          $(actionRow).prev().addClass('open')
         }
       }
     })
   })
-
-    //Make all cards in the same group the same height.
-
-    $(function() {
-        var cardGroup = $('.a-card-group');
-        var maxheight = 0;
-        cardGroup.children().each(function() {
-            if($(this).height() > maxheight) {
-                maxheight = $(this).height();
-            }
+  // Make all cards in the same group the same height
+  $(function () {
+    var cardGroup = $('.a-card-group'); var maxheight = 0
+    cardGroup.children().each(function () {
+      if ($(this).height() > maxheight) maxheight = $(this).height()
+    })
+    cardGroup.children().children().css('min-height', maxheight)
+  })
+  // Adjust position of second level menu upon click:
+  $(function () {
+    $('#colnav').on('mouseup', function (event) {
+      var target = $(event.target); var second = $('.a-colnav-secondLevel')
+      var getThird = function (el) {
+        if (el.attr('class') === '.a-colnav-thirdLevel') return el
+        else return el.find('.a-colnav-thirdLevel')
+      }
+      var findOpenThird = function (el) {
+        var bool = false
+        el.find('.a-colnav-thirdLevel').each(function () {
+          if ($(this).attr('data-ignore') === 'false') bool = true
         })
-        cardGroup.children().children().css("min-height", maxheight);
-    })
-
-    // Adjust position of second level menu upon click:
-    $(function () {
-      $('#colnav').on('mouseup', function (event) {
-        var target = $(event.target); var second = $('.a-colnav-secondLevel')
-        var getThird = function (el) {
-          if (el.attr('class') === '.a-colnav-thirdLevel') return el
-          else return el.find('.a-colnav-thirdLevel')
-        }
-        var findOpenThird = function (el) {
-          var bool = false
-          el.find('.a-colnav-thirdLevel').each(function () {
-            if ($(this).attr('data-ignore') === 'false') bool = true
-          })
-          return bool
-        }
-        var isOpen = function (el) {
-          var x = 'expanded'
-          return (el.closest('a').hasClass(x) || el.find('a').hasClass(x) ||
-            el.hasClass(x))
-        }
-        var ul = target.closest('ul')
-        if(ul.hasClass('a-colnav')) {
-            second.css('margin-left', '-1px')
-        }
-        if (ul.hasClass('a-colnav-secondLevel' || 'a-colnav-thirdLevel')) {
-          if (!findOpenThird(ul)) {
-              if($(window).width() >= bpLarge) {second.animate({ 'margin-left': '-78px' }, 125)}
+        return bool
+      }
+      var isOpen = function (el) {
+        var x = 'expanded'
+        return (el.closest('a').hasClass(x) || el.find('a').hasClass(x) ||
+          el.hasClass(x))
+      }
+      var ul = target.closest('ul')
+      if (ul.hasClass('a-colnav')) second.css('margin-left', '-1px')
+      if (ul.hasClass('a-colnav-secondLevel' || 'a-colnav-thirdLevel')) {
+        if (!findOpenThird(ul)) {
+          if ($(window).width() >= bpLarge) {
+            second.animate({ 'margin-left': '-78px' }, 125)
           }
-          else {
-              if($(window).width() >= bpLarge) {second.css('margin-left', '-78px') }
-          }
-          getThird(ul).css('margin-left', '-1px').css('left', '100%')
-            .attr('data-ignore', 'false')
-          if (ul.hasClass('a-colnav-secondLevel') && $(window).width() >= bpLarge) {
-            ul.children('li').children('a').addClass('dim-second')
-            target.closest('a').removeClass('dim-second')
-            target.children('a').removeClass('dim-second')
-            target.removeClass('dim-second')
-          }
-        } else if (ul.hasClass('a-colnav') && isOpen(target)) {
-            $('#a-js-suggestionList').css('display', 'block')
-          $('.dim').removeClass('dim'); second.css('margin-left', '-10000px')
-          getThird(ul).css('margin-left', '-10000px')
-            .attr('data-ignore', 'true')
-          $('.col-md-3').removeClass('col-md-3').addClass('col-md-6')
-            .removeClass('col-md-offset-4').addClass('col-md-offset-1')
         } else {
-          second.each(function () {
-            getThird($(this)).attr('data-ignore', 'true')
-          })
-          $('#a-js-suggestionList').css('display', 'none')
-          $('.dim-second').removeClass('dim-second')
-          $('.col-md-6').removeClass('col-md-6').addClass('col-md-3')
-            .removeClass('col-md-offset-1').addClass('col-md-offset-4')
+          if ($(window).width() >= bpLarge) second.css('margin-left', '-78px')
         }
-      })
+        getThird(ul).css('margin-left', '-1px').css('left', '100%')
+          .attr('data-ignore', 'false')
+        if (ul.hasClass('a-colnav-secondLevel') &&
+          $(window).width() >= bpLarge) {
+          ul.children('li').children('a').addClass('dim-second')
+          target.closest('a').removeClass('dim-second')
+          target.children('a').removeClass('dim-second')
+          target.removeClass('dim-second')
+        }
+      } else if (ul.hasClass('a-colnav') && isOpen(target)) {
+        $('#a-js-suggestionList').css('display', 'block')
+        $('.dim').removeClass('dim'); second.css('margin-left', '-10000px')
+        getThird(ul).css('margin-left', '-10000px').attr('data-ignore', 'true')
+        $('.col-md-3').removeClass('col-md-3').addClass('col-md-6')
+          .removeClass('col-md-offset-4').addClass('col-md-offset-1')
+      } else {
+        second.each(function () {
+          getThird($(this)).attr('data-ignore', 'true')
+        })
+        $('#a-js-suggestionList').css('display', 'none')
+        $('.dim-second').removeClass('dim-second')
+        $('.col-md-6').removeClass('col-md-6').addClass('col-md-3')
+          .removeClass('col-md-offset-1').addClass('col-md-offset-4')
+      }
     })
-
-  // INPUT Focus style
-  // If state on input is "focus", add class to a-input : "a-input-focus"
-
-  ; (function ($, undefined) {
-      $(function () {
-
-          $("input.form-control").focus(function(){
-          $(this).parent().addClass("a-input-focus");
-
-          }).blur(function(){
-                 $(this).parent().removeClass("a-input-focus");
-          })
-      });
-  }(jQuery));
-
-  // TOOLTIP AND POPOVERS
-
-  ; (function ($, undefined) {
-      $(function () {
-
-        $('[data-toggle="tooltip"]').tooltip();
-
-        $('[data-toggle="popover"]').popover();
-
-        $('#example').popover(options);
-
-      })
-  }(jQuery));
-
-
-  /*!
-   * COLAPSE "Mobile navigation" + add "open"-class
-   */
-  ; (function ($, undefined) {
-
-      $(function () {
-
-          $('.ap-sideNav-mobilebar').click(function () {
-              var self = $(this);
-              var searchButton = $('.a-toggle-search').hasClass('open');
-
-              if (self.hasClass('open')) {
-                  $('.ap-sideNav-collapse').slideUp(300);
-                  self.removeClass('open');
-              } else {
-                  if (searchButton === true) {
-                      $('.a-search').slideUp(300);
-                      $('.a-toggle-search').removeClass('open');
-
-                  }
-                  self.addClass('open');
-                  $('.ap-sideNav-collapse').slideDown(300);
-              }
-
-              return false;
-          })
-      });
-
-  }(jQuery));
-
-
-
-  // Add dim class to colnav FIRST LEVEL (The panels that is not active)
-  // TODO: Combine first and second level javascript. On second level, make the items not DIM when menu is closed and another 2-level is opened (Reset)
-
-  ; (function ($, undefined) {
-      $(function () {
-
-          $('.a-colnav-item').click(function () {
-             $(this).parent().find('.a-colnav-item-second').eq(0).focus() // Repairs drilldown navigation (keyboard/screen reader)
-             if ($(this).hasClass('expanded') && $(window).width() >= bpLarge) {
-                 $(this).removeClass('expanded');
-                 if ($('.a-colnav-item.expanded').length === 0) {
-                     $('.a-colnav-item').removeClass('dim-second-no');
-                 }
-                 else {
-                     $(this).addClass('dim')
-                 }
-             } else if($(window).width() >= bpLarge) {
-                 $('.a-colnav-item').removeClass('expanded');
-                 $(this).addClass('expanded');
-                 $('.a-colnav-item').addClass('dim');
-                 $('.a-colnav-item.expanded').removeClass('dim');
-             }
-         });
-      });
-  }(jQuery));
-
-  // Add dim class to colnav SECOND LEVEL
+  })
+  // If state on input is 'focus', add class to a-input: 'a-input-focus'
+  $(function () {
+    $('input.form-control').focus(function () {
+      $(this).parent().addClass('a-input-focus')
+    }).blur(function () { $(this).parent().removeClass('a-input-focus') })
+  })
+  // Tooltip and popovers
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover(); $('#example').popover(options)
+  })
+  // Collapse 'Mobile navigation' + add 'open'-class
+  $(function () {
+    $('.ap-sideNav-mobilebar').click(function () {
+      var self = $(this)
+      var searchButton = $('.a-toggle-search').hasClass('open')
+      if (self.hasClass('open')) {
+        $('.ap-sideNav-collapse').slideUp(300); self.removeClass('open')
+      } else {
+        if (searchButton === true) {
+          $('.a-search').slideUp(300); $('.a-toggle-search').removeClass('open')
+        }
+        self.addClass('open'); $('.ap-sideNav-collapse').slideDown(300)
+      }
+      return false
+    })
+  })
+  // Add dim class to colnav first level (the panels that are not active)
+  $(function () {
+    $('.a-colnav-item').click(function () {
+      $(this).parent().find('.a-colnav-item-second').eq(0).focus()
+      if ($(this).hasClass('expanded') && $(window).width() >= bpLarge) {
+        $(this).removeClass('expanded')
+        if ($('.a-colnav-item.expanded').length === 0) {
+          $('.a-colnav-item').removeClass('dim-second-no')
+        } else $(this).addClass('dim')
+      } else if ($(window).width() >= bpLarge) {
+        $('.a-colnav-item').removeClass('expanded')
+        $(this).addClass('expanded'); $('.a-colnav-item').addClass('dim')
+        $('.a-colnav-item.expanded').removeClass('dim')
+      }
+    })
+  })
+  // Add dim class to colnav second level
   $(function () {
     $('.a-colnav-item-second').click(function () {
-      $(this).parent().find('.a-colnav-item-third').eq(0).focus() // Repairs drilldown navigation (keyboard/screen reader)
-           if ($(this).hasClass('expanded-second') && $(window).width() >= bpLarge) {
-               $(this).removeClass('expanded-second');
-               if ($('.a-colnav-item-second.expanded-second').length === 0) {
-                   $('.a-colnav-item-second').removeClass('dim-second-no');
-               }
-               else {
-                   $(this).addClass('dim-second')
-               }
-           } else if($(window).width() >= bpLarge){
-               $('.a-colnav-item-second').removeClass('expanded-second');
-               $(this).addClass('expanded-second');
-               $('.a-colnav-item-second').addClass('dim-second');
-               $('.a-colnav-item-second.expanded-second').removeClass('dim-second');
+      $(this).parent().find('.a-colnav-item-third').eq(0).focus()
+      if ($(this).hasClass('expanded-second') && $(window).width() >= bpLarge) {
+        $(this).removeClass('expanded-second')
+        if ($('.a-colnav-item-second.expanded-second').length === 0) {
+          $('.a-colnav-item-second').removeClass('dim-second-no')
+        } else $(this).addClass('dim-second')
+      } else if ($(window).width() >= bpLarge) {
+        $('.a-colnav-item-second').removeClass('expanded-second')
+        $(this).addClass('expanded-second')
+        $('.a-colnav-item-second').addClass('dim-second')
+        $('.a-colnav-item-second.expanded-second').removeClass('dim-second')
       }
     })
   })
