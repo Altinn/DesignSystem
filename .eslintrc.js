caaafe6b7eeaac813c750650b2a31bf1afcec51f
Truls Clark <truls.clark@gmail.com>
2016-10-07 14:35:56 +0200

    Added eslint, airbnb-styleguide, and custom rules. Rules needs to be agreed upon.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": "airbnb-base/legacy",
+  "parserOptions": {
+    "ecmaVersion": 5
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "rules": {
+    "brace-style": ["error", "1tbs"],
+    "curly": ["error", "all"],
+    "func-call-spacing": ["error", "never"],
+    "linebreak-style": ["error", "windows"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
+    "space-before-function-paren": ["error", "never"]
+  }
+};
