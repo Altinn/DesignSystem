64263823b826bfd17ada5dfb8bd7c29d00bf0dd0
David Emanuelsen <david.emanuelsen@outlook.com>
2016-10-10 14:55:41 +0200

    #1249: Fixed linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "linebreak-style": ["error", "windows"],
     "quotes": ["error", "single"],
     "semi": ["error", "always"],
-    "space-before-function-paren": ["error", "never"]
+    "space-before-function-paren": ["error", "never"],
+    "func-names": ["error", "never"]
   }
 };
