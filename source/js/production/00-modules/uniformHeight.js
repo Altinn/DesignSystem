1589087b17e9b8e661a2f3a7bd2a3930f2050b39
MikaeI <mikael@persille.no>
2016-10-10 09:16:18 +0200

    Responsive fixes to uniform height script.

--- source/js/production/00-modules/uniformHeight.js
+++ source/js/production/00-modules/uniformHeight.js
@@ -1,8 +1,13 @@
 /* globals $ */
 var uniformHeight = function () {
-  var cardGroup = $('.a-card-group .container .row'); var maxheight = 0
-  cardGroup.children().each(function () {
-    if ($(this).height() > maxheight) maxheight = $(this).height()
-  })
-  cardGroup.children().children().css('min-height', maxheight)
+  var cardGroup = $('.a-card-group .container .row'); 
+  if ($(window).width() >= 768) {
+    var maxheight = 0
+    cardGroup.children().each(function () {
+      if ($(this).height() > maxheight) maxheight = $(this).height()
+    })
+    cardGroup.children().children().css('min-height', maxheight)
+  } else {
+    cardGroup.children().children().css('min-height', 'auto')
+  }
 }
