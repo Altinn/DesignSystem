fa219eb2aaab75b388b8c6f317882cf9390456e5
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2016-10-11 10:32:55 +0200

    state colors

--- js/altinnDev.js
+++ js/altinnDev.js
@@ -23,6 +23,17 @@ var insetVariations = function() {
 };
 
 /* globals $ */
+window.openGitMD = function (target) {
+  window.open('https://github.com/Altinn/DesignSystem/edit/master/source' +
+    $(target).closest('.sg-pattern').find('.patternLink')
+      .attr('href').replace(/\.\.\//g, '')
+      .replace('patterns', '/_patterns')
+      .replace(/(?:[^/*]*)$/, '').slice(0, -1)
+      .replace(/-([0-9]{2})/g, '/$1') + '.md', '_blank'
+  )
+}
+
+/* globals $ */
 var preOpenModals = function() {
   $('#modalExample').addClass('in');
 };
