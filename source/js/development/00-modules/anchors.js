81e84276138e165809e31808621526bcb27c666d
MikaeI <mikael@persille.no>
2016-10-12 09:24:41 +0200

    Anchors.

--- source/js/development/00-modules/anchors.js
+++ source/js/development/00-modules/anchors.js
@@ -1,6 +1,5 @@
-var anchors = function() {
-  // window.anchors.options.placement = 'left'
-  // window.anchors.options.class = 'a-anchor'
-  // window.anchors
-  //   .add('.ap-content h1, .ap-content h2, .ap-content h3, .ap-content h4')
+var _anchors = function() {
+  window.anchors.options.placement = 'left'
+  window.anchors.options.class = 'a-anchor'
+  window.anchors.add('h3')
 };
