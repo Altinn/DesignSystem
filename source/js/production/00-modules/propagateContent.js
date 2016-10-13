fbb5c3584a1975ead57ca3fa4b6153a6facb12df
MikaeI <mikael@persille.no>
2016-10-13 13:30:27 +0200

    Misc. small fixes.

--- source/js/production/00-modules/propagateContent.js
+++ source/js/production/00-modules/propagateContent.js
@@ -1,6 +1,6 @@
 /* globals $ */
 var propagateContent = function() {
-  $('.propagated-content-destination').each(function() {
+  $('.a-js-propagatedContentDestination').each(function() {
     var prefix = '.a-js-propagatedContentOrigin.';
     if ($(this).hasClass('replace-me')) {
       $(this).before($(prefix + $(this).attr('data-refclass')).html());
