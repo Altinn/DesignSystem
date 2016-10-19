c25ad18719d1f14bf89efb02a24c343b2d0a2773
MikaeI <mikael@persille.no>
2016-10-19 14:42:01 +0200

    JavaScript refactoring/moving JS code out of Mustache files.

--- source/js/development/init.js
+++ source/js/development/init.js
@@ -1,6 +1,76 @@
-/* globals _anchors, hideIntroInSubs, insetVariations, selectAll, toggleTheme, preOpenModals */
+/* globals _anchors, hideIntroInSubs, insetVariations, selectAll, toggleTheme,
+preOpenModals, $ */
 window.altinnDev = function() {
-  _anchors(); hideIntroInSubs(); insetVariations(); selectAll(); toggleTheme();
+  _anchors();
+  hideIntroInSubs();
+  insetVariations();
+  selectAll();
+  toggleTheme();
   preOpenModals();
 };
 window.altinnDev();
+$('.html-escape').each(function() {
+  $(this).text($(this).html());
+});
+window.smoothStateMod = function() {
+  $('#smoothState').find('a:not(.sr-only-focusable)').on('click', function(e) {
+    if (location.pathname.replace(/\//g, '')
+      === $(this).attr('href').replace(/\//g, '').replace(/\.\./g, '')) {
+      e.preventDefault(); e.stopPropagation();
+      e.stopImmediatePropagation();
+      return false;
+    } else if ($(this).attr('href') !== '#') {
+      e.preventDefault(); e.stopPropagation();
+      e.stopImmediatePropagation();
+      window.smoothState.load($(this).attr('href'));
+    }
+    return false;
+  });
+};
+$(document).foundation();
+window.smoothState = $('#smoothState').smoothState({
+  prefetch: true,
+  debug: true,
+  anchors: 'blink',
+  cacheLength: 9,
+  onStart: {
+    duration: 250,
+    render: function($container) {
+      $container.addClass('is-exiting');
+      window.smoothState.restartCSSAnimations();
+      if ($container.find('.a-tempAnim').length > 0) {
+        $container.find('.a-scene_element')
+          .removeClass('a-scene_element--fadeinright')
+          .removeClass('a-scene_element--fadeinleft')
+          .addClass('a-scene_element--fadeoutright');
+      } else {
+        $container.find('.a-scene_element')
+          .removeClass('a-scene_element--fadeinright')
+          .removeClass('a-scene_element--fadeinleft')
+          .addClass('a-scene_element--fadeoutleft');
+      }
+    }
+  },
+  onReady: {
+    duration: 0,
+    render: function($container, $newContent) {
+      $container.removeClass('is-exiting');
+      $newContent.find('.a-scene_element--fadeinright').hide();
+      if ($container.find('.a-tempAnim').length > 0) {
+        $newContent.find('.a-scene_element')
+          .removeClass('a-scene_element--fadeinright')
+          .addClass('a-scene_element--fadeinleft')
+          .removeClass('a-tempAnim');
+      }
+      $container.html($newContent);
+    }
+  },
+  onAfter: function() {
+    $(document).foundation();
+    window.altinnInit();
+    window.altinnDev();
+    window.smoothStateMod();
+    $('.a-scene_element').show();
+  }
+}).data('smoothState');
+window.smoothStateMod();
