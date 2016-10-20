3eb309ee2fabfb848d50d94880a4c63ceb147cc2
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2016-10-20 07:42:44 +0200

    update

--- js/altinnDev.js
+++ js/altinnDev.js
@@ -3,6 +3,12 @@ var _anchors = function() {
   window.anchors.options.class = 'a-sg-anchor';
   window.anchors.add('h3');
   window.anchors.remove('.sg-pattern-example h3');
+  window.anchors.remove('.a-page h1');
+  window.anchors.remove('.a-page h2');
+  window.anchors.remove('.a-page h3');
+  window.anchors.remove('.a-page h4');
+  window.anchors.remove('.a-page h5');
+  window.anchors.remove('.a-page h6');
 };
 
 /* globals $ */
--- js/altinnProd.js
+++ js/altinnProd.js
@@ -209,6 +209,40 @@ var handleFocus = function() {
 };
 
 /* globals $ */
+var initializeDatepicker = function() {
+  $('.form-control.date').datepicker({
+    format: 'dd.mm.yyyy',
+    language: 'no',
+    todayHighlight: true,
+    orientation: 'bottom left',
+    maxViewMode: 0
+  }).on('show', function(e) {
+    $('.datepicker').find('.next').html('');
+    $('.datepicker').find('.prev').html('');
+    $('.datepicker').find('table').attr('cellpadding', '0');
+    $('.datepicker').find('table').attr('cellspacing', '0');
+    $('.datepicker').each(function() {
+      if ($(this).find('.today').html().indexOf('<span') === -1) {
+        $(this).find('.today').html('<span>' + $(this).find('.today').html() + '</span>');
+      }
+      if ($(this).find('.active').html().indexOf('<span') === -1) {
+        $(this).find('.active').html('<span>' + $(this).find('.active').html() + '</span>');
+      }
+    });
+  });
+  $('.form-control.date').on('change', function() {
+    $('.datepicker').each(function() {
+      if ($(this).find('.today').html().indexOf('<span') === -1) {
+        $(this).find('.today').html('<span>' + $(this).find('.today').html() + '</span>');
+      }
+      if ($(this).find('.active').html().indexOf('<span') === -1) {
+        $(this).find('.active').html('<span>' + $(this).find('.active').html() + '</span>');
+      }
+    });
+  });
+};
+
+/* globals $ */
 var mobileNavigation = function() {
   $('.ap-sideNav-mobilebar').click(function() {
     var self = $(this);
@@ -368,7 +402,7 @@ var uniformHeight = function() {
 
 /* globals questionnaireInteraction, drilldownInteraction, handleFocus,
 mobileNavigation, propagateContent, toggleExpand, toggleFilter, uniformHeight,
-tooltip, popover, aTagSpaceExpand */
+tooltip, popover, aTagSpaceExpand, initializeDatepicker */
 window.altinnInit = function() {
   toggleExpand();
   drilldownInteraction();
@@ -381,5 +415,6 @@ window.altinnInit = function() {
   propagateContent();
   questionnaireInteraction();
   aTagSpaceExpand();
+  initializeDatepicker();
 };
 window.altinnInit();
