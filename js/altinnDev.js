d415e737e1dba08fca7a0dba0c2ddc980c84fe43
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2016-10-24 22:07:54 +0200

    update

--- js/altinnDev.js
+++ js/altinnDev.js
@@ -46,6 +46,19 @@ var preOpenModals = function() {
   $('#modalExample').addClass('in');
 };
 
+
+/* Prototyping that place will be visible after typing in postnumber */
+$(document).ready(function() {
+    $("#postnummer").keyup(function() {
+        if (this.value == "0123") {
+            $("#a-js-place").css("display", "inline");
+        }
+        else {
+            $("#a-js-place").css("display", "none");
+        }
+    });
+});
+
 /* globals $ */
 var selectAll = function() {
   var ctrlDown = false;
