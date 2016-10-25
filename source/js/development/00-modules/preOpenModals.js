c9083bd486bf5bcd88f0df35419aac3b9a0ca906
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2016-10-25 08:57:32 +0200

    fix lint error

--- source/js/development/00-modules/preOpenModals.js
+++ source/js/development/00-modules/preOpenModals.js
@@ -2,3 +2,14 @@
 var preOpenModals = function() {
   $('#modalExample').addClass('in');
 };
+
+/* Prototyping that place will be visible after typing in postnumber */
+$(document).ready(function() {
+  $('#postnummer').keyup(function() {
+    if (this.value === '0123') {
+      $('#a-js-place').css('display', 'inline');
+    } else {
+      $('#a-js-place').css('display', 'none');
+    }
+  });
+});
