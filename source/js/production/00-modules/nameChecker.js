425210004f8edc046bbacdede4e18b555c4badd2
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2016-10-27 19:35:40 +0200

    fix name checker on starte ENK

--- source/js/production/00-modules/nameChecker.js
+++ source/js/production/00-modules/nameChecker.js
@@ -6,7 +6,8 @@ var nameChecker = function() {
     .find('.a-btn').eq(0)
     .attr('onclick');
   var initAction = '$(".a-js-validator").find("input[type=text]")' +
-      '.attr("disabled", "disabled");' +
+      '.attr("disabled", "disabled").parent().addClass("disabled")' +
+      '.addClass("a-input-approved");' +
     '$(".a-js-validator").find(".a-validatorInfo").eq(0).hide();' +
     '$(".a-js-validator").find(".a-validatorInfo").eq(1).show();' +
     '$(".a-js-validator").find(".a-btn-group").find(".a-btn").eq(0)' +
@@ -21,7 +22,8 @@ var nameChecker = function() {
     text: 'Prøv et annet navn'
   }).appendTo('.a-btn-group', '.a-js-validator');
   $('.a-js-tryAnother').hide().on('click', function() {
-    $('.a-js-validator').find('input[type=text]').removeAttr('disabled');
+    $('.a-js-validator').find('input[type=text]').removeAttr('disabled')
+      .parent().removeClass('disabled').removeClass('a-input-approved');
     $('.a-js-tryAnother').hide();
     $('.a-js-validator').find('.a-validatorInfo').eq(0).show();
     $('.a-js-validator').find('.a-validatorInfo').eq(1).hide();
@@ -29,7 +31,7 @@ var nameChecker = function() {
       .html(btnText)
       .attr('onclick', initAction);
   });
-  $('.a-js-validator').find('.a-message-error').css('margin-bottom', '10px');
+  $('.a-js-validator').find('.a-message-error');
   function toggleBtns(el) {
     if ($(el).length > 0 && $(el).val().length > 0) {
       $('.a-js-validator').find('.a-btn-group').find('.a-btn').eq(0)
@@ -47,7 +49,8 @@ var nameChecker = function() {
         $('.a-js-validator').find('.a-btn-group').find('.a-btn').eq(0)
           .addClass('a-js-hideFromSmoothState');
         $('.a-js-validator').find('.a-btn-group').find('.a-btn').eq(0)
-          .attr('onclick', '');
+          .attr('onclick',
+            '$(".a-js-validator").find(".a-message-error").show()');
       }
     } else {
       $('.a-js-validator').find('.a-btn-group').find('.a-btn').eq(0)
@@ -64,6 +67,7 @@ var nameChecker = function() {
     toggleBtns(this);
   });
   $('.a-js-validator').find('input[type=text]').on('keydown', function(e) {
+    $('.a-js-validator').find('.a-message-error').hide();
     if (e.which === 13) {
       e.preventDefault(); e.stopPropagation();
       if ($('.a-js-validator').find('.a-btn-group').find('.a-btn').eq(0)
