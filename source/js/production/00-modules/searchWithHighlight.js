367b3c5179cd9b8b72dbcf1b7824deb6d0da7b04
David Emanuelsen <david.emanuelsen@outlook.com>
2016-10-31 10:42:15 +0100

    Fixed linting errors, added $ to global variables in lint config

--- source/js/production/00-modules/searchWithHighlight.js
+++ source/js/production/00-modules/searchWithHighlight.js
@@ -13,28 +13,30 @@ var executeSearch = function($searchInput, $targetTable) {
   $targetTable.find('tbody>tr').each(function() {
     var $currentRow = $(this);
     var match = false;
-    
+
     $(this).find('td[data-searchable]').each(function() {
-      var src_str = $(this).html();
+      var matches;
+      var pattern;
+      var srcString = $(this).html();
       var term = $searchInput.val();
 
       // Create a regex with the search term that ignores tags,
       // this enables the search text "test testing" to hit "<span>test</span> testing"
       term = term.replace(/(\s+)/, '(<[^>]+>)*$1(<[^>]+>)*');
-      var pattern = new RegExp('(' + term + ')', 'gi');
+      pattern = new RegExp('(' + term + ')', 'gi');
 
       // Only perform the highlighting if there are matches
-      var matches = pattern.exec(src_str);
+      matches = pattern.exec(srcString);
       if (matches && matches.length > 0) {
-        src_str = src_str.replace(pattern, '<mark>$1</mark>');
+        srcString = srcString.replace(pattern, '<mark>$1</mark>');
 
         // If a search for "test testing" made a hit in "<span>test</span> testing", the resulting
-        // html would look like "<span><mark>test</span> testing</mark>", but we want 
+        // html would look like "<span><mark>test</span> testing</mark>", but we want
         // "<span><mark>test</mark></span><mark> testing</mark>"
-        src_str = src_str.replace(/(<mark>[^<>]*)((<[^>]+>)+)([^<>]*<\/mark>)/, '$1</mark>$2<mark>$4');
+        srcString = srcString.replace(/(<mark>[^<>]*)((<[^>]+>)+)([^<>]*<\/mark>)/, '$1</mark>$2<mark>$4');
 
         // Update the column with the highlighted content, and register that a match was made
-        $(this).html(src_str);
+        $(this).html(srcString);
         match = true;
       }
     });
@@ -48,19 +50,22 @@ var executeSearch = function($searchInput, $targetTable) {
 
 var searchTableWithHighlight = function($searchInput, $targetTable) {
   $searchInput.on('input', function() {
+    var searchTerm = $(this).val();
     resetSearchTarget($targetTable);
 
     // Exit if no input has been given
-    var searchTerm = $(this).val();
     if (searchTerm.length === 0) {
       return false;
     }
-    
+
     executeSearch($searchInput, $targetTable);
+
+    return true;
   });
 };
 
-// Register the search-event for all input fields with the data attribute "search-algorithm" set to "show-and-highlight"
+// Register the search-event for all input fields with the data attribute
+// "search-algorithm" set to "show-and-highlight"
 $(document).on('ready', function() {
   $('input[data-search-algorithm="show-and-highlight"]').each(function() {
     searchTableWithHighlight($(this), $('#' + this.dataset.searchTarget));
