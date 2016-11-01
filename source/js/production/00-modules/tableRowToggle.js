0d5ac7e0a6cf2390a337d844f61c9d19a2841eae
mariavar <maria.aevarsd@gmail.com>
2016-11-01 14:21:53 +0100

    Added js styling functionality

--- source/js/production/00-modules/tableRowToggle.js
+++ source/js/production/00-modules/tableRowToggle.js
@@ -2,6 +2,11 @@ $(document).on('ready', function() {
   var $table = $('table[data-table-eventhandler="tableRowToggle"]');
   $table.on('click', 'tbody>tr', function() {
     $(this).toggleClass('selected');
+    if ($(this).hasClass('selected')) {
+      $(this).find('td:last-child').html('Angre <i class="material-icons a-table-icon undo-icon red">history </i>');
+    } else {
+      $(this).find('td:last-child').html('Fjern <i class="material-icons a-table-icon remove-icon">remove_circle</i>');
+    }
     if ($table.find('tr.selected').length > 0) {
       $('.segment-done').show();
     } else {
