984c334b91b9dc91c805844d285f4c5df7234d68
Tor Magnus Hånde <tor-magnus.hande@capgemini.com>
2016-11-08 13:20:00 +0100

    Finished 139-human-icon

--- source/js/production/00-modules/toggleSwitch.js
+++ source/js/production/00-modules/toggleSwitch.js
@@ -1,15 +1,33 @@
 $(document).on('ready', function() {
   var $allWithTarget = $('.switch-container input[data-switch-target]');
   var allTargets = [];
+
   $.each($allWithTarget, function() {
-    allTargets.push($(this).data('switch-target'));
+    $.each($(this).data('switch-target'), function() {
+      allTargets.push(this + '');
+    });
   });
 
   $allWithTarget.on('click', function() {
     var $currentSwitch = $(this);
-    $('#' + $currentSwitch.data('switch-target')).show();
+    var switchTargets = $currentSwitch.data('switch-target');
+
+    $.each(switchTargets, function() {
+      $('#' + this).show();
+    });
+
     $.each(allTargets, function() {
-      if ($currentSwitch.data('switch-target') !== this + '') {
+      var hide = true;
+      var outerTarget = this + '';
+
+      $.each(switchTargets, function() {
+        var innerTarget = this + '';
+        if (outerTarget === innerTarget) {
+          hide = false;
+        }
+      });
+
+      if (hide) {
         $('#' + this).hide();
       }
     });
