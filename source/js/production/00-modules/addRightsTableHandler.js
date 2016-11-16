4cc9ccc8f433220017d98a45eac6b54af8cd2eec
David Emanuelsen <david.emanuelsen@outlook.com>
2016-11-16 12:22:56 +0100

    Testing

--- source/js/production/00-modules/addRightsTableHandler.js
+++ source/js/production/00-modules/addRightsTableHandler.js
@@ -1,5 +1,24 @@
 var setupAddRightsHandler = function() {
   $('.a-js-confirmAddRightBtn').on('click', function() {
     $(this).closest('.a-collapseContent').prev().addClass('a-sortable-row-complete');
+
+    var $actionsRow = $($(this).closest('tr'));
+    console.log($actionsRow[0].dataset);
+    var targetTable = $actionsRow[0].dataset.targetTable;
+      console.log(targetTable);
+    if (targetTable) {
+      console.log(targetTable);
+
+      var toBeCopied = $actionsRow.prev().clone();
+      $(toBeCopied).find('.a-collapseTitle').toggleClass('toggle-collapse-text');
+
+      $actionsRow.prev().addClass('a-sortable-row-complete');
+
+      $('#' + targetTable + ' tbody').append(toBeCopied);
+      $('#' + targetTable + ' tbody').append($actionsRow);
+
+      toBeCopied.removeClass('a-sortable-row-complete');
+    }
+    toggleFilter();
   });
 };
---
+++ source/js/production/00-modules/tableRowCopy.js
@@ -0,0 +1,11 @@
+var copySiblingRowToTargetTable = function(src, targetTable) {
+//   var $actionsRow = $($(src).closest('tr'));
+//   var toBeCopied = $actionsRow.prev().clone();
+//   $(toBeCopied).find('.a-collapseTitle').toggleClass('toggle-collapse-text');
+
+//   $actionsRow.prev().addClass('a-sortable-row-complete');
+//   $('#' + targetTable + ' tbody').append(toBeCopied);
+//   $('#' + targetTable + ' tbody').append($actionsRow);
+
+//   toBeCopied.removeClass('a-sortable-row-complete');
+};
