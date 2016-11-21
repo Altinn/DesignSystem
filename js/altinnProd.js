868cd0b23f2a28e31491f9bba7011217e0deb10d
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2016-11-21 12:28:48 +0100

    link to prototype

--- js/altinnProd.js
+++ js/altinnProd.js
@@ -35,6 +35,16 @@ var setupAddRightsHandler = function() {
   });
 };
 
+$(document).on('ready', function() {
+  var clipboard = new Clipboard('.a-js-btnCopy');
+  clipboard.on('success', function(e) {
+      console.log('s', e);
+  });
+  clipboard.on('error', function(e) {
+      console.log(e);
+  });
+});
+
 /* globals $ */
 var codeLookup = function() {
   var capIt = function(string) {
