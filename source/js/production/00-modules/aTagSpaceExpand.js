5485cef233e339b365faf7a49f1aa55ac48bee0d
MikaeI <mikael@persille.no>
2016-11-28 11:48:45 +0100

    Bugfixes.

--- source/js/production/00-modules/aTagSpaceExpand.js
+++ source/js/production/00-modules/aTagSpaceExpand.js
@@ -4,7 +4,7 @@ var aTagSpaceExpand = function() {
     $(this).on('keydown', function(e) {
       if (e.keyCode === 32 || e.keycode === 13 || e.which === 32 || e.which === 13) {
         e.stopPropagation(); e.preventDefault();
-        $(e.target).trigger($.event('click'));
+        $(e.target).trigger('click');
       }
     });
   });
