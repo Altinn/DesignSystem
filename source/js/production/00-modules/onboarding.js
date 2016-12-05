131948b8c23be252a79cc255d188151734a8d631
MikaeI <mikael@persille.no>
2016-12-05 11:57:00 +0100

    Bugfixes.

--- source/js/production/00-modules/onboarding.js
+++ source/js/production/00-modules/onboarding.js
@@ -5,10 +5,28 @@ var onboarding = function() {
   $('body').addClass($('.a-js-bodyClassPersist').attr('data-body'));
   $.get('../../images/flashlight.svg', function(data) {
     var steps = onboardingCrawl();
-    $('.a-page').append('<span class="onboarding-wrapper"><span ' +
-      'class="a-js-circle"></span>' + xmlToString(data) + '</span>' +
-      '<span class="onboarding-neutral">' + xmlToString(data) + '</span>'
-    );
+    if ($('body').width() > 450) {
+      $('.a-page').append('<span class="onboarding-wrapper"><span ' +
+        'class="a-js-circle"></span>' + xmlToString(data) + '</span>' +
+        '<span class="onboarding-neutral">' + xmlToString(data) + '</span>'
+      );
+    } else {
+      $('.a-page').append('<span class="onboarding-wrapper"><span ' +
+        'class="a-js-circle"></span>' +
+          xmlToString(data)
+            .replace('width="1140"', 'width="' + $('body').width() + '"') +
+        '</span>' +
+        '<span class="onboarding-neutral">' +
+          xmlToString(data)
+            .replace('width="1140"', 'width="' + $('body').width() + '"') +
+        '</span>'
+      );
+      $('.onboarding-wrapper').css('width', $('body').width() + 'px');
+      $('.onboarding-neutral').css('width', $('body').width() + 'px');
+      $('.onboarding-neutral').css('transform', 'scale(1)');
+      // $('.a-onboarding').css('width', $('body').width() + 'px');
+      $('.a-onboarding').css('top', '-260px');
+    }
     $('body').css('overflow-x', 'hidden');
     $('.onboarding-neutral').hide();
     $('.onboarding-wrapper').hide();
@@ -22,7 +40,18 @@ var onboarding = function() {
         }
         count += 1;
         if (steps[count]) {
-          onboardingSeek(count, steps);
+          onboardingSeek(count, steps, function(ratio) {
+            onboardingStep(count + 1, function() {
+              // console.log(ratio);
+              // var width = $('body').width() / ratio;
+              // $('.onboarding-wrapper').css('width', width + 'px');
+              // $('.onboarding-neutral').css('width', width + 'px');
+              // $('.onboarding-wrapper').html('<span class="a-js-circle">' +
+              //   xmlToString(data)
+              //     .replace('width="1140"', 'width="' + width + '"')
+              //   + '</span>');
+            });
+          });
         } else {
           $('.a-js-circle').css('opacity', '0.9'); onboardingBlank(true, 450);
         }
