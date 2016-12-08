c7be6c6bf9458fc597c1b0088e7d5cf09a9adcba
MikaeI <mikael@persille.no>
2016-12-08 12:45:56 +0100

    Onboarding fixes.

--- source/js/production/00-modules/onboarding.js
+++ source/js/production/00-modules/onboarding.js
@@ -1,16 +1,18 @@
 /* globals $, onboardingStep, onboardingOnShow, onboardingSeek, onboardingHide,
   onboardingCrawl, onboardingBlank */
 var onboarding = function() {
-  var count = -1; var steps = onboardingCrawl();
+  var count = -1; var steps;
   $('body').addClass($('.a-js-bodyClassPersist').attr('data-body'));
   $('.a-page').append('<span class="onboarding-wrapper"><span ' +
     'class="a-js-circle"></span></span><span class="onboarding-neutral"></span>'
   );
-  if ($('body').width() < 576) {
+  if ($('body').width() < 768) {
     $('.onboarding-wrapper').css('position', 'fixed');
     $('.onboarding-neutral').css('position', 'fixed');
     $('.a-onboarding').css('top', '-260px');
+    $('.navbar-toggler')[0].click();
   }
+  steps = onboardingCrawl();
   $('body').css('overflow-x', 'hidden');
   $('.onboarding-neutral').hide();
   $('.onboarding-wrapper').hide();
@@ -22,13 +24,16 @@ var onboarding = function() {
       onboardingHide(onboardingStep(0));
     } else {
       if (count === -1) {
-        $('.a-js-circle').css('opacity', '0');
+        $('.a-js-circle').css('background-color', 'rgba(2, 47, 81, 0)');
+      } else if ($('body').width() < 768 && count === 1) {
+        $('.navbar-toggler')[0].click();
       }
       count += 1;
       if (steps[count]) {
         onboardingSeek(count, steps);
       } else {
-        $('.a-js-circle').css('opacity', '0.9'); onboardingBlank(true, 576);
+        $('.a-js-circle').css('background-color', 'rgba(2, 47, 81, 1)');
+        onboardingBlank(true, 450);
       }
       onboardingStep(count + 1);
     }
@@ -39,6 +44,9 @@ var onboarding = function() {
       onboardingHide(onboardingStep(0));
     } else {
       count = -1; onboardingBlank(true); onboardingStep(0);
+      if ($('body').width() < 768) {
+        $('.navbar-toggler')[0].click();
+      }
     }
   });
   $('.a-js-modalLauncher').each(function() {
