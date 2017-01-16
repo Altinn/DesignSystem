6810c2815c3bba409ca799086045f16e20383220
David Emanuelsen <david.emanuelsen@outlook.com>
2017-01-16 13:47:48 +0100

    Changed client delegation panel to use the new list (still some pending changes on the search javascript)

--- source/js/production/00-modules/searchWithHighlight.js
+++ source/js/production/00-modules/searchWithHighlight.js
@@ -11,34 +11,62 @@ var mark = function() {
 
   $.each(this.dataset.searchTarget.split(','), function() {
     // Reset visibility of all rows
-    var table = '#' + this.toString();
-    $(table + ' tbody>tr:not(.a-js-ignoreDuringSearch)').show();
-    $(table).find('tfoot').hide();
+    var target = '#' + this.toString();
+    if ($(target).prop('nodeName') == 'table') {
+      $(target + ' tbody>tr:not(.a-js-ignoreDuringSearch)').show();
+      $(target).find('tfoot').hide();
 
-    // Show information row if open before search
-    $(table + ' tbody>tr.open:not(.a-js-ignoreDuringSearch)').each(function() {
-      $(this).next().show();
-      $(table).find('tfoot').hide();
-    });
-
-    $(table).find('tbody>tr>td[data-searchable="true"]').unmark().mark(input, options);
-
-     // Hide unmarked rows
-    if (input.length > 0) {
-      $(table + ' tbody>tr:not(.a-js-ignoreDuringSearch)').each(function() {
-        if ($(this).has('td mark').length === 0) {
-          $(this).hide();
-          // Hide preceding information row
-          if ($(this).next().hasClass('a-collapseContent')) {
-            $(this).next().hide();
-          }
-        }
+      // Show information row if open before search
+      $(target + ' tbody>tr.open:not(.a-js-ignoreDuringSearch)').each(function() {
+        $(this).next().show();
+        $(target).find('tfoot').hide();
       });
 
-      if ($(table + ' tbody>tr:not(.a-js-ignoreDuringSearch):visible').length === 0) {
-        $(table).find('tfoot').show();
-      } else {
-        $(table).find('tfoot').hide();
+      $(target).find('tbody>tr>td[data-searchable="true"]').unmark().mark(input, options);
+
+      // Hide unmarked rows
+      if (input.length > 0) {
+        $(target + ' tbody>tr:not(.a-js-ignoreDuringSearch)').each(function() {
+          if ($(this).find('mark').length === 0) {
+            $(this).hide();
+            console.log('Hiding');
+            // Hide preceding information row
+            if ($(this).next().hasClass('a-collapseContent')) {
+              $(this).next().hide();
+            }
+          }
+        });
+
+        if ($(target + ' tbody>tr:not(.a-js-ignoreDuringSearch):visible').length === 0) {
+          $(target).find('tfoot').show();
+        } else {
+          $(target).find('tfoot').hide();
+        }
+      }
+    } else {
+      $(target + ' li:not(.a-js-ignoreDuringSearch):not(.a-list-header)').show();
+
+      // Show information row if open before search
+      // TODO for list
+      // $(target + ' tbody>tr.open:not(.a-js-ignoreDuringSearch)').each(function() {
+      //   $(this).next().show();
+      //   $(target).find('tfoot').hide();
+      // });
+
+      $(target).find('*[data-searchable="true"]').unmark().mark(input, options);
+
+      // Hide unmarked rows
+      if (input.length > 0) {
+        $(target + ' li:not(.a-js-ignoreDuringSearch):not(.a-list-header)').each(function() {
+          if ($(this).find('mark').length === 0) {
+            $(this).hide();
+            // Hide preceding information row
+            // TODO for list
+            // if ($(this).next().hasClass('a-collapseContent')) {
+            //   $(this).next().hide();
+            // }
+          }
+        });
       }
     }
   });
