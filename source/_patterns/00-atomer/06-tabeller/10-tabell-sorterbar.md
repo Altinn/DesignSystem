27426e76a8627002ba579ca0f87768f459d03858
VORONKOVA Olga <olga.voronkova@soprasteria.com>
2017-01-23 14:53:25 +0100

    2169 cleanup unused tables

--- source/_patterns/00-atomer/06-tabeller/10-tabell-sorterbar.md
+++
@@ -1,5 +0,0 @@
----
- state: indesignreview
- version: 1
----
-Eksempel på sorterbar tabell med mulighet for å gjennomføre en handling per rad. Raden får da egen styling og en tekst som bekreftelse på at handlingen har blitt gjennomført.
--- source/js/production/00-modules/sortableRowExpand.js
+++
@@ -1,7 +0,0 @@
-var setupSortableRowOnclick = function() {
-  $('.a-js-sortable-clickable-row').on('click', function() {
-    if (!$(this).hasClass('a-sortable-row-complete') && !$(this).hasClass('a-sortable-row-deleted')) {
-      $(this).find('a[data-toggle]')[0].click();
-    }
-  });
-};
