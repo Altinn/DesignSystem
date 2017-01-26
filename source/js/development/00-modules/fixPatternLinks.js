cbb3c169c71cf683d3973a1d504f4476b25335c5
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-01-26 07:39:22 +0100

    link fix

--- source/js/development/00-modules/fixPatternLinks.js
+++ source/js/development/00-modules/fixPatternLinks.js
@@ -2,7 +2,9 @@
 var fixPatternLinks = function() {
   if (window.location.pathname.indexOf('DesignSystem') === 1) {
     $('a').each(function() {
-      $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/patterns/'));
+      if (typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href').indexOf('DesignSystem') === -1) {
+        $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/patterns/'));
+      }
     });
     $('*[onclick]').each(function() {
       if ($(this).attr('onclick').indexOf('location.href=\'/patterns/') > -1) {
