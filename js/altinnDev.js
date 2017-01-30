79904776068e495ac93d802b4b67bade419eef33
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-01-30 12:33:11 +0100

    update

--- js/altinnDev.js
+++ js/altinnDev.js
@@ -13,18 +13,18 @@ var _anchors = function() {
 
 /* globals $ */
 var fixPatternLinks = function() {
- if (window.location.pathname.indexOf('DesignSystem') === 1) {
-   $('a').each(function() {
-     if (typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href').indexOf('DesignSystem') === -1) {
-       $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/patterns/'));
-     }
-     });
-   $('*[onclick]').each(function() {
-     if ($(this).attr('onclick').indexOf('location.href=\'/patterns/') > -1) {
-       $(this).attr('onclick', $(this).attr('onclick').replace('location.href=\'/patterns/', 'location.href=\'/DesignSystem/patterns/'));
-     }
-   });
- }
+  if (window.location.pathname.indexOf('DesignSystem') === 1) {
+    $('a').each(function() {
+      if (typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href').indexOf('DesignSystem') === -1) {
+        $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/patterns/'));
+      }
+    });
+    $('*[onclick]').each(function() {
+      if ($(this).attr('onclick').indexOf('location.href=\'/patterns/') > -1) {
+        $(this).attr('onclick', $(this).attr('onclick').replace('location.href=\'/patterns/', 'location.href=\'/DesignSystem/patterns/'));
+      }
+    });
+  }
 };
 
 /* globals $ */
