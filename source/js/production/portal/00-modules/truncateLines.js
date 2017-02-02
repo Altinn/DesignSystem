c44acff4828aa39326af1a9e78104121cf4e53a2
David Emanuelsen <david.emanuelsen@outlook.com>
2017-02-02 09:30:57 +0100

    Fixing lint errors.....

--- source/js/production/portal/00-modules/truncateLines.js
+++ source/js/production/portal/00-modules/truncateLines.js
@@ -1,14 +1,14 @@
 var truncateToNumberOfLines = function(element) {
-  var p=$($(element).find('div')[0]);
-  var divh=$(element).height();
+  var p = $($(element).find('div')[0]);
+  var divh = $(element).height();
   if ($(p).outerHeight() >= (divh + 5)) {
     while ($(p).outerHeight() >= (divh + 5)) {
-      $(p).text(function (index, text) {
+      $(p).text(function(index, text) {
         return text.replace(/\W*\s(\S)*$/, '...');
       });
     }
   }
-}
+};
 
 var truncateBoxButtonNames = function() {
   $('.a-box-button').on('click', function() {
