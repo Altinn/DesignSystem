ec8d0b7e76ba2f95dc1aaa4acddb0621855290ca
MikaeI <mikael@persille.no>
2017-02-13 09:51:32 +0100

    Fixed propagated content functionality.

--- source/js/production/shared/00-modules/propagateContent.js
+++ source/js/production/shared/00-modules/propagateContent.js
@@ -2,11 +2,30 @@
 var propagateContent = function() {
   $('.a-js-propagatedContentDestination').each(function() {
     var prefix = '.a-js-propagatedContentOrigin.';
-    if ($(this).hasClass('replace-me')) {
+    if ($(this)[0].hasAttribute('data-maxwidth')) {
+      if (window.innerWidth <= parseInt($(this).attr('data-maxwidth'), 10)) {
+        if ($(this).hasClass('replace-me')) {
+          $(this).before($(prefix + $(this).attr('data-refclass')).html());
+          $(this).remove();
+        } else {
+          $(this).html($(prefix + $(this).attr('data-refclass')).html());
+        }
+      }
+    } else if ($(this)[0].hasAttribute('data-minwidth')) {
+      if (window.innerWidth >= parseInt($(this).attr('data-minwidth'), 10)) {
+        if ($(this).hasClass('replace-me')) {
+          $(this).before($(prefix + $(this).attr('data-refclass')).html());
+          $(this).remove();
+        } else {
+          $(this).html($(prefix + $(this).attr('data-refclass')).html());
+        }
+      }
+    } else if ($(this).hasClass('replace-me')) {
       $(this).before($(prefix + $(this).attr('data-refclass')).html());
       $(this).remove();
     } else {
       $(this).html($(prefix + $(this).attr('data-refclass')).html());
     }
   });
+  $('.a-js-propagatedContentOrigin').html('');
 };
