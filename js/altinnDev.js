0dfadd0956d06a42138bb458c40465301fcd4944
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-02-27 12:37:26 +0100

    font

--- js/altinnDev.js
+++ js/altinnDev.js
@@ -1346,11 +1346,22 @@ var handleValidatorLibrary = function() {
   });
 };
 
+var wasDark = $('header').hasClass('a-darkBackground');
 if ($('body').width() < 768) {
   $('.navbar-toggler').on('click', function() {
     if ($('.a-globalNav-main').hasClass('show')) {
+      if (wasDark) {
+        $('header').addClass('a-darkBackground');
+        $('.a-globalNav-logo').find('img')
+          .attr('src', $('.a-globalNav-logo').find('img').attr('src').replace('blue', 'white'));
+      }
       $('.a-page').children(':not(header)').removeClass('a-js-hidden');
     } else {
+      if (wasDark) {
+        $('header').removeClass('a-darkBackground');
+        $('.a-globalNav-logo').find('img')
+          .attr('src', $('.a-globalNav-logo').find('img').attr('src').replace('white', 'blue'));
+      }
       $('.a-page').children(':not(header)').addClass('a-js-hidden');
     }
   });
