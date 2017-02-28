75d6187e7cbd442ca689ef95381883dadd159808
MikaeI <mikael@persille.no>
2017-02-28 10:57:34 +0100

    Bugfixes

--- source/js/production/infoportal/00-modules/personSwitcher.js
+++ source/js/production/infoportal/00-modules/personSwitcher.js
@@ -1,5 +1,6 @@
 if ($('.a-js-personSwitcherTriggerOutside').length > 0) {
   $('body').on('mouseup', function(e) {
+    $('.a-page').children().not('header').removeClass('a-js-hidden');
     if ($(e.target).closest('.a-dropdown-personswitchList').length === 0 &&
       $(e.target).closest('.a-js-personSwitcherTriggerOutside').length === 0) {
       if ($('.a-dropdown-personswitchList').is(':visible')) {
@@ -13,7 +14,6 @@ if ($('.a-js-personSwitcherTriggerOutside').length > 0) {
         if ($('.a-globalNav-main').is(':visible')) {
           $('.navbar-toggler').trigger('click');
         }
-        $('.a-page').children().not('header').removeClass('a-js-hidden');
       }
     }
   });
