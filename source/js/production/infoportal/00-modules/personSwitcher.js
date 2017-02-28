b0674baaa5da8a2363f5832e4833f7e10e2d8ec7
MikaeI <mikael@persille.no>
2017-02-28 10:09:07 +0100

    Bugfixes

--- source/js/production/infoportal/00-modules/personSwitcher.js
+++ source/js/production/infoportal/00-modules/personSwitcher.js
@@ -1,22 +1,19 @@
 if ($('.a-js-personSwitcherTriggerOutside').length > 0) {
-  $('.a-js-personSwitcherTriggerOutside').on('click', function() {
-    if ($('.a-globalNav-main').hasClass('show')) {
-      $('.navbar-toggler').trigger('click');
-    }
-    $('.a-page').children().not('header').removeClass('a-js-hidden');
-    setTimeout(function() {
-      if (!$('.a-dropdown-personswitchList').is(':visible')) {
-        $('.a-dropdown-personswitchList').show();
-      } else {
-        $('.a-dropdown-personswitchList').hide();
-      }
-    }, 0);
-  });
   $('body').on('mouseup', function(e) {
     if ($(e.target).closest('.a-dropdown-personswitchList').length === 0 &&
       $(e.target).closest('.a-js-personSwitcherTriggerOutside').length === 0) {
       if ($('.a-dropdown-personswitchList').is(':visible')) {
         $('.a-dropdown-personswitchList').hide();
+      }
+    } else if ($(e.target).closest('.a-js-personSwitcherTriggerOutside').length > 0) {
+      if ($('.a-dropdown-personswitchList').is(':visible')) {
+        $('.a-dropdown-personswitchList').hide();
+      } else {
+        $('.a-dropdown-personswitchList').show();
+        if ($('.a-globalNav-main').is(':visible')) {
+          $('.navbar-toggler').trigger('click');
+        }
+        $('.a-page').children().not('header').removeClass('a-js-hidden');
       }
     }
   });
