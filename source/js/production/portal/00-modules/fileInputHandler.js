5aabfaf170bbae51a73747650989c28c0f415f9c
VORONKOVA Olga <olga.voronkova@soprasteria.com>
2017-03-07 09:51:37 +0100

    4183 klientdelegering pa fil changes

--- source/js/production/portal/00-modules/fileInputHandler.js
+++ source/js/production/portal/00-modules/fileInputHandler.js
@@ -1,9 +1,17 @@
 var onFileInputChange = function() {
+  $('.a-js-fileInputChangeHandler').on('focus', function() {
+    $('.a-js-fileInputChangeHandler').closest('label').addClass('a-custom-fileupload--focused');
+  });
+  $('.a-js-fileInputChangeHandler').on('blur', function() {
+    $('.a-js-fileInputChangeHandler').closest('label').removeClass('a-custom-fileupload--focused');
+  });
+
   $('.a-js-fileInputChangeHandler').on('change', function() {
     var $parent = $(this).parent();
+    var $fileListContainer = $parent.next();
     $parent.hide();
-    $parent.next().show();
-    $parent.next().find('.a-js-listItemText').text($(this).val().split('\\')[$(this).val().split('\\').length - 1]);
+    $fileListContainer.show();
+    $fileListContainer.find('.a-js-listItemText').text($(this).val().split('\\')[$(this).val().split('\\').length - 1]);
   });
 };
 
@@ -11,5 +19,5 @@ var onFileListDeleteClick = function(src) {
   var $fileListContainer = $(src).closest('.a-js-fileListContainer');
   $fileListContainer.prev().find('input').val('');
   $fileListContainer.hide();
-  $fileListContainer.prev().show();
+  $fileListContainer.prev().show(); // show file upload btn
 };
