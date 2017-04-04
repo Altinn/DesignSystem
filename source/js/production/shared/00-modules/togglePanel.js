80392a1f5c200b52d70d4796a3332630d6930488
VORONKOVA Olga <olga.voronkova@soprasteria.com>
2017-04-04 15:49:44 +0200

    5470 changes after design review, screen reader, hover, expanded icon

--- source/js/production/shared/00-modules/togglePanel.js
+++ source/js/production/shared/00-modules/togglePanel.js
@@ -2,14 +2,17 @@ $('.a-collapsePanel-body').on('show.bs.collapse', function() {
   var that = this;
   setTimeout(function() {
     var $collapsePanelHeader = $(that).siblings('.a-js-index-heading').first();
-    $collapsePanelHeader.find('.a-inboxHeadingContent')
-      .removeClass('a-msgUnread')
-      .find('.a-msgIconSecondary')
-      .closest('.a-msgIconWrapper')
-      .find('.ai')
+    var $msgIconWrapper = $collapsePanelHeader.find('.a-inboxHeadingContent')
+    .find('.a-msgIconSecondary')
+    .closest('.a-msgIconWrapper');
+
+    $msgIconWrapper.find('.ai')
       .hide()
       .siblings('.a-msgIconSecondary')
       .show();
+
+    $msgIconWrapper.find('span').attr('aria-hidden', true);
+    $msgIconWrapper.find('span:last-of-type').removeAttr('aria-hidden');
 
     $('.a-collapsePanel').removeClass('expanded');
     $(that).closest('.a-collapsePanel').addClass('expanded');
@@ -22,6 +25,7 @@ $('.a-collapsePanel-body').on('hide.bs.collapse', function() {
   var that = this;
   setTimeout(function() {
     var $collapsePanelHeader = $(that).siblings('.a-js-index-heading').first();
+    $collapsePanelHeader.find('.a-inboxHeadingContent').removeClass('a-msgUnread');
     $(that).closest('.a-collapsePanel').removeClass('expanded');
     if ($('.a-collapsePanel.expanded').length === 0) {
       $('.a-js-index-heading').removeClass('dim');
