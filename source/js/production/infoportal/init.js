68dc34180b88d876fdf54fae9c9513d942d420d2
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-04-19 22:44:21 +0200

    removed ellipsis maxTwoLines and replaced with truncate, also used by portal

--- source/js/production/infoportal/init.js
+++ source/js/production/infoportal/init.js
@@ -20,15 +20,3 @@ window.infoportalInit = function() {
 };
 window.infoportalInit();
 // $(document).foundation();
-$(document).ready(function() {
-  setTimeout(function() {
-    $('.a-maxTwoLines').ellipsis({
-      lines: 2,
-      responsive: true
-    });
-    $('.a-maxThreeLines').ellipsis({
-      lines: 3,
-      responsive: true
-    });
-  }, 0);
-});
--- source/js/production/shared/00-modules/setupTruncateLines.js
+++ source/js/production/shared/00-modules/setupTruncateLines.js
@@ -1,5 +1,10 @@
 var setupTruncateLines = function() {
   setTimeout(function() {
+    // Max two lines for all screen sizes
+    $('.a-js-truncate-2').truncate({
+      lines: 2
+    });
+    // Max two lines for screen sizes less than 768
     // Intit with 3 lines instead of 2 for IE11
     if (!!window.MSInputMethodContext
       && !!document.documentMode
@@ -15,6 +20,11 @@ var setupTruncateLines = function() {
   }, 1);
 
   $(window).resize(function() {
+    // Max two lines for all screen sizes
+    $('.a-js-truncate-2').truncate('collapse');
+    $('.a-js-truncate-2').truncate('update');
+
+    // Max two lines for screen sizes less than 768
     if (window.innerWidth < 768) {
       $('.a-js-truncate-2-sm-down').truncate('collapse');
       $('.a-js-truncate-2-sm-down').truncate('update');
