e58fb5144c4d8e6c48a7cb5c6bc6fe9a45426bf0
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-06-02 13:08:44 +0200

    small design fixes

--- source/js/production/infoportal/00-modules/questionnaireInteraction.js
+++ source/js/production/infoportal/00-modules/questionnaireInteraction.js
@@ -3,6 +3,7 @@ var questionnaireInteraction = function() {
   $('.a-trigger-question').each(function() {
     $(this).find('input').on('change', function() {
       $(this).parent().parent().parent()
+        .parent()
         .next()
         .css('display', 'block');
     });
