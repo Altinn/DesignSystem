0691015226d1844d3cd82f768f43329080b26e77
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-06-13 13:17:15 +0200

    jumpnav, help article in quickhelp, animation hint on button for opening quickhelp

--- source/js/production/infoportal/00-modules/altinnQuickhelp.js
+++ source/js/production/infoportal/00-modules/altinnQuickhelp.js
@@ -151,5 +151,9 @@ AltinnQuickhelp = {
         });
       }
     });
+    if ($('.quickhelpPage').find('.a-text').length !== 0) {
+      $('.quickhelpPage').parent('.a-page').addClass('a-page-hasArticleInside')
+    };
   }
+
 };
