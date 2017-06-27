daf849b54e3f86424496703fd9255e110488d834
MikaeI <mikael@persille.no>
2017-06-27 15:05:36 +0200

    Better solution for inline JS.

--- source/js/production/infoportal/init.js
+++ source/js/production/infoportal/init.js
@@ -19,6 +19,12 @@ window.infoportalInit = function() {
   setupFormValidation();
   autoFootnotes();
   AltinnQuickhelp.init();
+  $('body').on('focus', '#contactForm', function() {
+    setupFormValidation('#contactForm', '#a-js-contactForm-submit');
+  });
+  $('body').on('focus', '#contactForm2', function() {
+    setupFormValidation('#contactForm2', '#a-js-contactForm-submit2');
+  });
 };
 window.infoportalInit();
 // $(document).foundation();
