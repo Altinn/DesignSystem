a8a77072d1befa5bcebfbbccf5529cd3f9e01fb1
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-09-15 12:10:14 +0200

    dev

--- dist/js/infoportal.js
+++ dist/js/infoportal.js
@@ -706,37 +706,35 @@ var initializeDatepicker = function() {
       rightArrow: '<i class="ai ai-arrowright"></i>'
     }
   }).on('show', function(e) {
-    // $('.datepicker').find('.next').html('');
-    // $('.datepicker').find('.prev').html('');
     $('.datepicker').find('table').attr('cellpadding', '0');
     $('.datepicker').find('table').attr('cellspacing', '0');
-    $('.datepicker').each(function() {
-      if ($(this).find('.today').html().indexOf('<span') === -1) {
-        $(this).find('.today')
-          .html('<span>' + $(this).find('.today').html() + '</span>');
-      }
-      if ($(this).find('.active').html().indexOf('<span') === -1) {
-        $(this).find('.active')
-          .html('<span>' + $(this).find('.active').html() + '</span>');
-      }
-    });
+    // $('.datepicker').each(function() {
+    //   if ($(this).find('.today').html().indexOf('<span') === -1) {
+    //     $(this).find('.today')
+    //       .html('<span>' + $(this).find('.today').html() + '</span>');
+    //   }
+    //   if ($(this).find('.active').html().indexOf('<span') === -1) {
+    //     $(this).find('.active')
+    //       .html('<span>' + $(this).find('.active').html() + '</span>');
+    //   }
+    // });
   });
   if ($('.form-control.date').length > 0) {
     $('body').on('click', function(e) {
       $('.datepicker').hide();
     });
-    $('.form-control.date').on('change', function() {
-      $('.datepicker').each(function() {
-        if ($(this).find('.today').html().indexOf('<span') === -1) {
-          $(this).find('.today')
-          .html('<span>' + $(this).find('.today').html() + '</span>');
-        }
-        if ($(this).find('.active').html().indexOf('<span') === -1) {
-          $(this).find('.active')
-          .html('<span>' + $(this).find('.active').html() + '</span>');
-        }
-      });
-    });
+    // $('.form-control.date').on('change', function() {
+    //   $('.datepicker').each(function() {
+    //     if ($(this).find('.today').html().indexOf('<span') === -1) {
+    //       $(this).find('.today')
+    //       .html('<span>' + $(this).find('.today').html() + '</span>');
+    //     }
+    //     if ($(this).find('.active').html().indexOf('<span') === -1) {
+    //       $(this).find('.active')
+    //       .html('<span>' + $(this).find('.active').html() + '</span>');
+    //     }
+    //   });
+    // });
     $('.form-control.date').datepicker('setDate', new Date());
     $('.form-control.date').on('click', function(e) {
       e.stopPropagation(); e.preventDefault();
--- js/altinnDev.js
+++ js/altinnDev.js
@@ -5713,39 +5713,41 @@ var initializeDatepicker = function() {
     orientation: 'bottom left',
     autoclose: true,
     maxViewMode: 0,
-    container: '#picker-container'
+    container: '#picker-container',
+    templates: {
+      leftArrow: '<i class="ai ai-back"></i>',
+      rightArrow: '<i class="ai ai-arrowright"></i>'
+    }
   }).on('show', function(e) {
-    $('.datepicker').find('.next').html('');
-    $('.datepicker').find('.prev').html('');
     $('.datepicker').find('table').attr('cellpadding', '0');
     $('.datepicker').find('table').attr('cellspacing', '0');
-    $('.datepicker').each(function() {
-      if ($(this).find('.today').html().indexOf('<span') === -1) {
-        $(this).find('.today')
-          .html('<span>' + $(this).find('.today').html() + '</span>');
-      }
-      if ($(this).find('.active').html().indexOf('<span') === -1) {
-        $(this).find('.active')
-          .html('<span>' + $(this).find('.active').html() + '</span>');
-      }
-    });
+    // $('.datepicker').each(function() {
+    //   if ($(this).find('.today').html().indexOf('<span') === -1) {
+    //     $(this).find('.today')
+    //       .html('<span>' + $(this).find('.today').html() + '</span>');
+    //   }
+    //   if ($(this).find('.active').html().indexOf('<span') === -1) {
+    //     $(this).find('.active')
+    //       .html('<span>' + $(this).find('.active').html() + '</span>');
+    //   }
+    // });
   });
   if ($('.form-control.date').length > 0) {
     $('body').on('click', function(e) {
       $('.datepicker').hide();
     });
-    $('.form-control.date').on('change', function() {
-      $('.datepicker').each(function() {
-        if ($(this).find('.today').html().indexOf('<span') === -1) {
-          $(this).find('.today')
-          .html('<span>' + $(this).find('.today').html() + '</span>');
-        }
-        if ($(this).find('.active').html().indexOf('<span') === -1) {
-          $(this).find('.active')
-          .html('<span>' + $(this).find('.active').html() + '</span>');
-        }
-      });
-    });
+    // $('.form-control.date').on('change', function() {
+    //   $('.datepicker').each(function() {
+    //     if ($(this).find('.today').html().indexOf('<span') === -1) {
+    //       $(this).find('.today')
+    //       .html('<span>' + $(this).find('.today').html() + '</span>');
+    //     }
+    //     if ($(this).find('.active').html().indexOf('<span') === -1) {
+    //       $(this).find('.active')
+    //       .html('<span>' + $(this).find('.active').html() + '</span>');
+    //     }
+    //   });
+    // });
     $('.form-control.date').datepicker('setDate', new Date());
     $('.form-control.date').on('click', function(e) {
       e.stopPropagation(); e.preventDefault();
