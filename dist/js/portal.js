22d9222a5bd4592d94a32a98e212d0d72f65c89e
JulianHidalgo <julian.hidalgo@gmail.com>
2017-11-02 13:49:39 +0100

    iOS 11 bug workaround refactoring

--- dist/js/portal.js
+++ dist/js/portal.js
@@ -788,39 +788,42 @@ var enableIOS11Fix = function() {
   // document.body.style.height = '100%';
   // document.body.style.width = '100%';
 };
+
 var disableIOS11Fix = function() {
   document.body.style.overflow = 'auto';
   document.body.style.position = 'static';
-  // document.body.style.height = '100%';
-  // document.body.style.width = '100%';
 };
 
-var iOS11BugWorkAround = function() {
-  $('.a-stickyHelp-close').on('click', function() {
-    // When the close button in the sticky help window is clicked/tapped
-    // we disable the fix, otherwise the page will not scroll
-    window.parent.disableIOS11Fix();
-  });
-};
-
-$(document).ready(function() {
-  // Detect iOS 11_x affected by cursor position bug
-  // Bug report: https://bugs.webkit.org/show_bug.cgi?id=176896
+var isAffectedPlatform = function() {
   // Needs to be updated if new versions are affected
   var ua = navigator.userAgent;
   var iOS = /iPad|iPhone|iPod/.test(ua);
   var iOS11 = /OS 11_/.test(ua);
 
-  // Only apply this in the parent page, not in the modal
-  if (iOS && iOS11) {
+  return (iOS && iOS11);
+};
+
+var iOS11BugWorkAround = function() {
+  // Detect iOS 11_x affected by cursor position bug
+  // Bug report: https://bugs.webkit.org/show_bug.cgi?id=176896
+  if (isAffectedPlatform()) {
+    // This should run in the parent page only, not in the modal
     if ($('body.a-stickyHelp-body').length === 0) {
       // We enable the fix only when the help button is clicked/tapped
       $('.a-stickyHelp-open').on('click', function() {
         enableIOS11Fix();
       });
     }
+
+    // This should be running inside the iframe
+    $('.a-stickyHelp-close').on('click', function() {
+      // When the close button in the sticky help window is clicked/tapped
+      // we disable the fix, otherwise the page will not scroll
+      window.parent.disableIOS11Fix();
+    });
   }
-});
+};
+
 
 /* globals compareTo */
 var sortListAlphanumerically = function(src, sortIndex) {
