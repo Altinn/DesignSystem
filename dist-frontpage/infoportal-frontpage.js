f67efac923f2f0368caadeb9fdff27f69528da94
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-11-03 10:56:26 +0100

    javascript with contactForm and fonts renamed to font

--- dist-frontpage/infoportal-frontpage.js
+++ dist-frontpage/infoportal-frontpage.js
@@ -1381,6 +1381,27 @@ var iOS11BugWorkAround = function() {
 };
 
 
+/* globals $ */
+var contactForm = function() {
+  var $contactFormTrigger = $('#contact-form-trigger');
+
+  if ($contactFormTrigger.length > 0) {
+    $contactFormTrigger.on('click', function() {
+      var ScontactFormLink;
+      var fallbackUrl = $(this).data('fallback-url');
+
+      if (window.parent.$) {
+        ScontactFormLink = window.parent.$('#contact-form-link');
+        if (ScontactFormLink.length > 0) {
+          ScontactFormLink.click();
+        } else if (fallbackUrl) {
+          window.parent.location = fallbackUrl;
+        }
+      }
+    });
+  }
+};
+
 /*
   globals
   AltinnDropdown,
