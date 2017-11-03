6044463f5565a996aa9392f27d14a9636defec5d
Julián Hidalgo <julian.hidalgo@gmail.com>
2017-11-03 13:46:23 +0100

    Changes to "Skrive til oss link" behavior after test with Kristian

--- dist/js/portal.js
+++ dist/js/portal.js
@@ -629,23 +629,19 @@ var compareTo = function(firstItem, secondItem) {
 
 /* globals $ */
 var contactForm = function() {
-  var $contactFormTrigger = $('#contact-form-trigger');
+  $('body.a-stickyHelp-body').on('click', '#contact-form-trigger', function() {
+    var ScontactFormLink;
 
-  if ($contactFormTrigger.length > 0) {
-    $contactFormTrigger.on('click', function() {
-      var ScontactFormLink;
-      var fallbackUrl = $(this).data('fallback-url');
-
-      if (window.parent.$) {
-        ScontactFormLink = window.parent.$('#contact-form-link');
-        if (ScontactFormLink.length > 0) {
-          ScontactFormLink.click();
-        } else if (fallbackUrl) {
-          window.parent.location = fallbackUrl;
-        }
+    if (window.parent.$) {
+      ScontactFormLink = window.parent.$('#contact-form-link');
+      if (ScontactFormLink.length > 0) {
+        ScontactFormLink.click();
+        return false;
       }
-    });
-  }
+    }
+
+    return true;
+  });
 };
 
 var setupExpandContent = function() {
