375781f54e17d3edd889b2d6d763c76493ad91ef
Bjørg Komso <bdk@brreg.no>
2017-11-06 15:50:12 +0100

    implementation of switch dropdown

---
+++ source/_patterns/00-atomer/03-knapper/21-knapp-link-brreg.md
@@ -0,0 +1,7 @@
+---
+state: inprogress brreg
+version: 1
+---
+NB: Dette er en variant av [knapp.mustache]
+
+Denne knappen er laget for å teste at visning basert på state og valgt tema fungerer
--- styleguidekit-assets-altinn/src/js/switch-project-nav.js
+++ styleguidekit-assets-altinn/src/js/switch-project-nav.js
@@ -1,17 +1,83 @@
+function getLocalStorageValue(key) {
+  return window.localStorage.getItem(key);
+}
+
+function setLocalStorageValue(key, value) {
+  window.localStorage.setItem(key, value);
+}
+
+function getSelectedProject() {
+  return getLocalStorageValue('selected_project');
+}
+
+function setSelectedProject(value) {
+  setLocalStorageValue('selected_project', value);
+}
+
+$.fn.toggleProjectComponent = function(hide) {
+  if ($(this).is('a')) {     // Gjemmer menypunkt
+    if (hide === true) {
+      $(this).hide();
+    } else {
+      $(this).show();
+    }
+  } else {
+    if (hide === true) {
+      $(this).closest('.sg-pattern').hide(); // Gjemmer div på siden med html
+    } else {
+      $(this).closest('.sg-pattern').show();
+    }
+  }
+};
+
+$.fn.hasAnyProjectStateClass = function(availableProjects) {
+  var i;
+  for (i = 0; i < availableProjects.length; i += 1) {
+    if ($(this).hasClass(availableProjects[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var checkComponentElements = function(elements) {
+  var availableProjects = ['altinn', 'brreg', 'altinnett'];
+  var project = getSelectedProject();
+  if (getSelectedProject() === null) {
+        setSelectedProject('altinn');
+  }
+  $.each(elements, function(index, element) {
+    if ($(element).hasAnyProjectStateClass(availableProjects)) {
+            // ligger prosjektdef på staten til patternet i md-fila
+      if ($(element).hasClass(project)) {   // samsvar mellom valgt theme og staten på patternet
+        $(element).toggleProjectComponent(false);
+      } else {
+        $(element).toggleProjectComponent(true);
+      }
+    }
+  });
+};
+
+var removeComponentsNotRelevantForProject = function() {
+  var allHeaderElements = document.querySelectorAll('.sg-pattern-state');
+  var iframeElements = document.querySelector('#sg-viewport').contentDocument.querySelectorAll('.sg-pattern-state');
+  checkComponentElements(allHeaderElements);
+  checkComponentElements(iframeElements);
+};
+
 $(document).ready(function() {
+  removeComponentsNotRelevantForProject();
+  $('.selLabel').click(function() {
+    $('.dropdown').toggleClass('active');
+  });
 
-    $(".selLabel").click(function () {
-        $('.dropdown').toggleClass('active');
-    });
+  $('.dropdown-list li').click(function() {
+    var selected = $(this).text();
+    $('.selLabel').html($(this).html());
+    $('.dropdown').removeClass('active');
+    window.localStorage.setItem('selected_project', $(this).attr('data-value'));
+    removeComponentsNotRelevantForProject();
+  });
 
-    $(".dropdown-list li").click(function() {
-        var selected = $(this).text();
-        console.log('You Selected: ' + $(this).attr("data-value"));
-        $('.selLabel').html($(this).html());
-        $('.dropdown').removeClass('active');
-        window.localStorage.setItem('selected_project', $(this).attr("data-value"));
-        console.log('Project selection ' + window.localStorage.getItem('selected_project') + ' saved to localStorage')
-    });
-
-    $(".dropdown-list li:first-child").click();
+  $('.dropdown-list li:first-child').click();
 });
