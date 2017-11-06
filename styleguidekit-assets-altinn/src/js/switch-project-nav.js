fc86b62b4260f542e938eb1a286cc20ac083925b
Marcos Figueiredo <maf@brreg.no>
2017-11-06 11:29:56 +0100

    Updating of gulp js task

--- styleguidekit-assets-altinn/src/js/switch-project-nav.js
+++ styleguidekit-assets-altinn/src/js/switch-project-nav.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
 
     $(".dropdown-list li").click(function() {
         var selected = $(this).text();
-        console.log('You Selected: ' + $(this).attr("data-value"));
+        /*console.log('You Selected: ' + $(this).attr("data-value"));*/
         $('.selLabel').html($(this).html());
         $('.dropdown').removeClass('active');
-        window.localStorage.setItem('selected_project', $(this).attr("data-value"));
-        console.log('Project selection ' + window.localStorage.getItem('selected_project') + ' saved to localStorage')
+        /*window.localStorage.setItem('selected_project', $(this).attr("data-value"));
+        console.log('Project selection ' + window.localStorage.getItem('selected_project') + ' saved to localStorage')*/
     });
 
     $(".dropdown-list li:first-child").click();
