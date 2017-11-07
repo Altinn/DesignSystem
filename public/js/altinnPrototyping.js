077a90c06706b11376ee383eeb27e7e51106080b
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-11-07 15:03:45 +0100

    fix path issue

--- public/js/altinnPrototyping.js
+++ public/js/altinnPrototyping.js
@@ -312,12 +312,12 @@ var fixPatternLinks = function() {
   if (window.location.pathname.indexOf('DesignSystem') === 1) {
     $('a').each(function() {
       if (typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href').indexOf('DesignSystem') === -1) {
-        $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/patterns/'));
+        $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/public/patterns/'));
       }
     });
     $('*[onclick]').each(function() {
       if ($(this).attr('onclick').indexOf('location.href=\'/patterns/') > -1) {
-        $(this).attr('onclick', $(this).attr('onclick').replace('location.href=\'/patterns/', 'location.href=\'/DesignSystem/patterns/'));
+        $(this).attr('onclick', $(this).attr('onclick').replace('location.href=\'/patterns/', 'location.href=\'/DesignSystem/public/patterns/'));
       }
     });
   }
