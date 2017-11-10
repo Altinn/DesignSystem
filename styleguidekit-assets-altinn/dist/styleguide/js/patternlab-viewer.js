6ff569f8986726b8152012984222dbd7103c4d5e
Marcos Figueiredo <maf@brreg.no>
2017-11-10 13:06:07 +0100

    BRREG Atom CSS effect

--- styleguidekit-assets-altinn/dist/styleguide/js/patternlab-viewer.js
+++ styleguidekit-assets-altinn/dist/styleguide/js/patternlab-viewer.js
@@ -2036,7 +2036,42 @@ var pluginLoader = {
 
 pluginLoader.init();
 
+var resetSwitchLayout = function() {
+  var $elementToReset = 'ul.a-sg-switch-dropdown-list';
+  var $elementLiToReset = $elementToReset + ' li';
+  $($elementLiToReset).each(function() {
+    if ($(this).css('display') !== null) {
+      $(this).removeAttr('style');
+    }
+  });
+  $($elementToReset).removeClass(window.localStorage.getItem('selected_project'));
+  $($elementLiToReset).each(function() {
+    if ($(this).css('display') !== null) {
+      $(this).removeAttr('style');
+    }
+  });
+  $($elementToReset).removeClass(window.localStorage.getItem('selected_project'));
+};
 
+var updateDropdownLayout = function(selectedProject) {
+  var switchClass = 'ul.a-sg-switch-dropdown-list';
+  switch (selectedProject) {
+  case 'altinn':
+    $(switchClass + ' li').eq(0).css('display', 'none');
+    $(switchClass).addClass('altinn');
+    break;
+  case 'brreg':
+    $(switchClass + ' li').eq(1).css('display', 'none');
+    $(switchClass).addClass('brreg');
+    break;
+  case 'altinnett':
+    $(switchClass + ' li').eq(2).css('display', 'none');
+    $(switchClass).addClass('altinnett');
+    break;
+  default:
+    break;
+  }
+};
 
 function getLocalStorageValue(key) {
   return window.localStorage.getItem(key);
@@ -2078,185 +2113,130 @@ $.fn.hasAnyProjectStateClass = function(availableProjects) {
   return false;
 };
 
-var checkComponentElements = function(elements) {
+function checkAndChangeComponentElements(project, elements) {
   var availableProjects = ['altinn', 'brreg', 'altinnett'];
-  var project = getSelectedProject();
   if (getSelectedProject() === null) {
     setSelectedProject('altinn');
     $('.display-altinnett').hide();
     $('.display-brreg').hide();
   }
   $.each(elements, function(index, element) {
+      // Checks if there is any project theme set on the state of the compoenent
     if ($(element).hasAnyProjectStateClass(availableProjects)) {
-            // ligger prosjektdef på staten til patternet i md-fila
-      if ($(element).hasClass(project)) {   // samsvar mellom valgt theme og staten på patternet
+      if ($(element).hasClass(project)) {
+          // there is set a state that is the same as the choosen theme
         $(element).toggleProjectComponent(false);
       } else {
         $(element).toggleProjectComponent(true);
       }
     }
   });
-};
+}
 
-var removePagesAndTemplatesFromNav = function() {
-  var project = getSelectedProject();
-  $('a').filter('.sg-acc-handle').show();
-  console.log('removePagesAndTemplatesFromNav');
+function removePagesAndTemplatesFromNav(project) {
+  var $patternTypeLinks = $('li a.sg-acc-handle');
+  var projectLinksToHide = [];
+  $patternTypeLinks.show();
   switch (project) {
   case 'altinn':
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-brreg');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-brreg');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-altinnett');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-altinnett');
-    }).hide();
+    projectLinksToHide = ['maler-brreg', 'sider-brreg', 'maler-altinnett', 'sider-altinnett'];
+    $patternTypeLinks.each(function() {
+      if ($.inArray($(this).text().toLowerCase(), projectLinksToHide) !== -1) {
+        $(this).hide();
+      }
+    });
     break;
-
   case 'brreg':
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-infoportal');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-infoportal');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-altinnett');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-altinnett');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-portal');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-portal');
-    }).hide();
+    projectLinksToHide = ['maler-infoportal', 'sider-infoportal', 'maler-altinnett', 'sider-altinnett', 'maler-portal', 'sider-portal'];
+    $patternTypeLinks.each(function() {
+      if ($.inArray($(this).text().toLowerCase(), projectLinksToHide) !== -1) {
+        $(this).hide();
+      }
+    });
     break;
   case 'altinnett' :
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-brreg');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-brreg');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-infoportal');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-infoportal');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'maler-portal');
-    }).hide();
-    $('a').filter(function() {
-      return ('.sg-acc-handle' && $(this).text().toLowerCase() === 'sider-portal');
-    }).hide();
-
-  }
-};
-
-var changeCss = function() {
-  var project = getSelectedProject();
-  var $head = $('#sg-viewport').contents().find('head link[rel=\'stylesheet\']');
-  switch (project) {
-  case 'altinn':
-    $('#sg-viewport').contents().find('head link[href~=\'/css/style.dist.altinnett.css\']').remove();
-    $('#sg-viewport').contents().find('head link[href~=\'/css/style.dist.brreg.css\']').remove();
-    break;
-  case 'altinnett':
-    $head.last().after('<link rel=\'stylesheet\' href=\'/css/style.dist.altinnett.css\' type=\'text/css\' media=\'screen\'>');
-    $('#sg-viewport').contents().find('head link[href~=\'/css/style.dist.brreg.css\']').remove();
-    break;
-  case 'brreg':
-    $head.last().after('<link rel=\'stylesheet\' href=\'/css/style.dist.brreg.css\' type=\'text/css\' media=\'screen\'>');
-    $('#sg-viewport').contents().find('head link[href~=\'/css/style.dist.altinnett.css\']').remove();
-    break;
-  }
-};
-
-var removeComponentsNotRelevantForProject = function() {
-  var allHeaderElements = document.querySelectorAll('.sg-pattern-state');
-  var iframeElements = document.querySelector('#sg-viewport').contentDocument.querySelectorAll('.sg-pattern-state');
-  checkComponentElements(allHeaderElements);
-  checkComponentElements(iframeElements);
-  removePagesAndTemplatesFromNav();
-  changeCss();
-};
-
-$('#sg-viewport').load(function() {   // iframe
-  removeComponentsNotRelevantForProject();
-});
-
-
-var resetSwitchLayout = function() {
-  console.log('Resetting switch layout ...');
-  $('ul.switch-dropdown-list li').each(function() {
-    if ($(this).css('display') !== null) {
-      $(this).removeAttr('style');
-    }
-  });
-  $('ul.switch-dropdown-list').removeClass(window.localStorage.getItem('selected_project'));
-};
-
-var updateDropdownLayout = function(selectedProject) {
-  'use strict';
-
-  var switchClass = 'ul.switch-dropdown-list';
-  console.log('Updating project project switch based on ' + selectedProject + ' option ...');
-
-  switch (selectedProject) {
-  case 'altinn':
-    console.log('Processing option altinn');
-    $(switchClass + ' li').eq(0).css('display', 'none');
-    $(switchClass).addClass('altinn');
-    break;
-  case 'brreg':
-    console.log('option brreg');
-    $(switchClass + ' li').eq(1).css('display', 'none');
-    $(switchClass).addClass('brreg');
-    break;
-  case 'altinnett':
-    console.log('option altinnett');
-    $(switchClass + ' li').eq(2).css('display', 'none');
-    $(switchClass).addClass('altinnett');
+    projectLinksToHide = ['maler-brreg', 'sider-brreg', 'maler-infoportal', 'sider-infoportal', 'maler-portal', 'sider-portal'];
+    $patternTypeLinks.each(function() {
+      if ($.inArray($(this).text().toLowerCase(), projectLinksToHide) !== -1) {
+        $(this).hide();
+      }
+    });
     break;
   default:
-    console.error('Selected project [' + selectedProject + '] not found');
     break;
   }
-};
+}
+
+function changeCss(project) {
+  var $viewPortContents = $('#sg-viewport').contents();
+  var $viewPortContentsHead = $viewPortContents.find('head link[rel=\'stylesheet\']');
+  switch (project) {
+  case 'altinnett':
+    $viewPortContentsHead.last().after('<link rel=\'stylesheet\' href=\'/css/style.dist.altinnett.css\' type=\'text/css\' media=\'screen\'>');
+    $viewPortContents.find('head link[href~=\'/css/style.dist.brreg.css\']').remove();
+    break;
+  case 'brreg':
+    $viewPortContentsHead.last().after('<link rel=\'stylesheet\' href=\'/css/style.dist.brreg.css\' type=\'text/css\' media=\'screen\'>');
+    $viewPortContents.contents().find('head link[href~=\'/css/style.dist.altinnett.css\']').remove();
+    break;
+  default:
+    $viewPortContents.find('head link[href~=\'/css/style.dist.altinnett.css\']').remove();
+    $viewPortContents.find('head link[href~=\'/css/style.dist.brreg.css\']').remove();
+    break;
+  }
+}
+
+function toggleWelcomeText(project) {
+  var $viewPortContents = $('#sg-viewport').contents();
+  if (project === 'brreg') {
+    $viewPortContents.find('.welcome-panel-altinn').hide();
+    $viewPortContents.find('.welcome-panel-brreg').show();
+  } else {
+    $viewPortContents.find('.welcome-panel-brreg').hide();
+    $viewPortContents.find('.welcome-panel-altinn').show();
+  }
+}
+
+function changeContentNotRelevantForProject() {
+  var allHeaderElements = document.querySelectorAll('.sg-pattern-state');
+  var iframeElements = document.querySelector('#sg-viewport').contentDocument.querySelectorAll('.sg-pattern-state');
+  var project = getSelectedProject();
+  if (getSelectedProject() === null) {
+    setSelectedProject('altinn');
+  }
+  checkAndChangeComponentElements(project, allHeaderElements);
+  checkAndChangeComponentElements(project, iframeElements);
+  removePagesAndTemplatesFromNav(project);
+  changeCss(project);
+  toggleWelcomeText(project);
+}
+
+$('#sg-viewport').load(function() {   // iframe
+  changeContentNotRelevantForProject();
+});
 
 $(document).ready(function() {
-  'use strict';
-
-  var switchClass = '.switch-dropdown';
-  $('.selLabel').click(function() {
-    $('.switch-dropdown').toggleClass('active');
+  var $switchClass = '.a-sg-switch-dropdown';
+  var $switchLabelClass = $switchClass + ' .a-sg-selLabel';
+  $($switchLabelClass).click(function() {
+    $($switchClass).toggleClass('active');
   });
 
-  $(switchClass + '-list li').click(function() {
+  $($switchClass + '-list li').click(function() {
     var selected = $(this).attr('data-value');
     resetSwitchLayout();
 
-    $(switchClass + ' .selLabel').text($(this).text());
-    $(switchClass).removeClass('active');
-    window.localStorage.setItem('selected_project', selected);
+    $($switchLabelClass).text($(this).text());
+    $($switchClass).removeClass('active');
+    setLocalStorageValue('selected_project', selected);
     updateDropdownLayout(selected);
-    removeComponentsNotRelevantForProject();
-    console.log('Local storage value: ' + getSelectedProject());
+    changeContentNotRelevantForProject();
   });
   if (getSelectedProject() === null) {
-    $(switchClass + '-list li:first-child').click();
+    $($switchClass + '-list li:first-child').click();
   } else {
-    $(switchClass + ' .selLabel').text($(switchClass + '-list li #project-' + getSelectedProject()).text());
+    $($switchLabelClass).text($($switchClass + '-list li #project-' + getSelectedProject()).text());
     updateDropdownLayout(getSelectedProject());
   }
-  console.log('Local storage value: ' + getSelectedProject());
 });
 
