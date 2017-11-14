5257f820d4791cc6202b1f3fed4e33686a371ab0
Martin Svendsen <martin.svendsen@soprasteria.com>
2017-11-14 12:47:26 +0100

    Fix list padding when toggling

--- source/js/prototyping/modules/toggleSavedEdit.js
+++ source/js/prototyping/modules/toggleSavedEdit.js
@@ -2,7 +2,11 @@
 var toggleSavedEdit = function() {
   $(function() {
     $('.a-js-toggleEdit').on('click', function() {
-      $('.js-edit').toggleClass('d-none');
+      $('.js-edit').toggleClass('invisible');
+    });
+    $('.js-edit').on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
     });
   });
 };
