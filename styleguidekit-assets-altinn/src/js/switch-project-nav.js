3546f80ec61fcf9deb66e4a1cbf34a2f5c4707c8
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-11-28 13:45:14 +0100

    started splitting altinn and common styles into different folders

--- styleguidekit-assets-altinn/src/js/switch-project-nav.js
+++ styleguidekit-assets-altinn/src/js/switch-project-nav.js
@@ -139,17 +139,17 @@ function changeCss(project) {
   case 'altinn':
     $viewPortContents.find('head link[href~=\'../../css/style.dist.brreg.css\']').prop('disabled', true);
     $viewPortContents.find('head link[href~=\'../../css/style.dist.altinnett.css\']').prop('disabled', true);
-    $viewPortContents.find('head link[href~=\'../../css/style.css\']').prop('disabled', false);
+    $viewPortContents.find('head link[href~=\'../../css/style.dist.altinn.css\']').prop('disabled', false);
     break;
   case 'altinnett':
     $viewPortContents.find('head link[href~=\'../../css/style.dist.brreg.css\']').prop('disabled', true);
     $viewPortContents.find('head link[href~=\'../../css/style.dist.altinnett.css\']').prop('disabled', false);
-    $viewPortContents.find('head link[href~=\'../../css/style.css\']').prop('disabled', true);
+    $viewPortContents.find('head link[href~=\'../../css/style.dist.altinn.css\']').prop('disabled', true);
     break;
   case 'brreg':
     $viewPortContents.find('head link[href~=\'../../css/style.dist.brreg.css\']').prop('disabled', false);
     $viewPortContents.find('head link[href~=\'../../css/style.dist.altinnett.css\']').prop('disabled', true);
-    $viewPortContents.find('head link[href~=\'../../css/style.css\']').prop('disabled', true);
+    $viewPortContents.find('head link[href~=\'../../css/style.dist.altinn.css\']').prop('disabled', true);
     break;
   default:
     break;
