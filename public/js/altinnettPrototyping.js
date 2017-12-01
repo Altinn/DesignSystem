8436225eb37b7b1910b61bfb2c70b3eb711737c3
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-12-01 08:57:30 +0100

    public with patternlink fix for altinnett

--- public/js/altinnettPrototyping.js
+++ public/js/altinnettPrototyping.js
@@ -1,3 +1,19 @@
+/* globals $ */
+var fixPatternLinks = function() {
+  if (window.location.pathname.indexOf('DesignSystem') === 1) {
+    $('a').each(function() {
+      if (typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href').indexOf('DesignSystem') === -1) {
+        $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/public/patterns/'));
+      }
+    });
+    $('*[onclick]').each(function() {
+      if ($(this).attr('onclick').indexOf('location.href=\'/patterns/') > -1) {
+        $(this).attr('onclick', $(this).attr('onclick').replace('location.href=\'/patterns/', 'location.href=\'/DesignSystem/public/patterns/'));
+      }
+    });
+  }
+};
+
 var setupTruncateLines = function() {
   setTimeout(function() {
     // Max two lines for all screen sizes
@@ -630,10 +646,14 @@ AltinnModal = {
 };
 
 /* globals
+  fixPatternLinks,
+
   AltinnModal
   setupTruncateLines
 */
 window.altinnettInit = function() {
+  fixPatternLinks();
+
   AltinnModal.init();
   setupTruncateLines();
 };
