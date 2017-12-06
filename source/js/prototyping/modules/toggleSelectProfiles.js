a60f45033783f38eb3b9e94fde52608a98eef960
Bjørn Langfors <bjorn@langfors.no>
2017-12-06 07:59:42 +0100

    - Substitute "small-checkboxes" with discreet variant (fixed in portal) (#96)
    - Update button texts for actor selection
    - Add "with subunits" option for actor selection
    - Increase indentation of subunits displayed in actor selection

--- source/js/prototyping/modules/toggleSelectProfiles.js
+++ source/js/prototyping/modules/toggleSelectProfiles.js
@@ -3,7 +3,7 @@ var toggleSelectProfiles = function() {
   $('#selectedProfiles').hide();
   $('#profile-selection').hide();
 
-  $('#alle-jeg-kan-representere-checkbutton-1').on('click', function() {
+  $('#alle-jeg-kan-representere-checkbutton-1,#alle-jeg-kan-representere-checkbutton-3').on('click', function() {
     $('#profile-selection').hide();
   });
   $('#select-profile-checkbutton-2').on('click', function() {
