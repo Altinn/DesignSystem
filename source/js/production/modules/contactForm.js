7c8828b06a043960502e22a8316876c410683cbb
Julian Hidalgo <julian.hidalgo@gmail.com>
2017-12-19 18:02:05 -0300

    #13353 - Quick help must disappear when clicking “Skriv til oss" and modal appears

--- source/js/production/modules/contactForm.js
+++ source/js/production/modules/contactForm.js
@@ -2,11 +2,14 @@
 var contactForm = function() {
   $('body.a-stickyHelp-body').on('click', '#contact-form-trigger', function() {
     var ScontactFormLink;
+    var $closeButton;
 
     if (window.parent.$) {
       ScontactFormLink = window.parent.$('#contact-form-link');
+      $closeButton = window.parent.$('.a-stickyHelp-close');
       if (ScontactFormLink.length > 0) {
         ScontactFormLink.click();
+        $closeButton.click();
         return false;
       }
     }
