cba2289b49a4e0155f12964fd02598efd2dada1b
Dhanalakshmi Gopalswamy <34273718+acn-dgopa@users.noreply.github.com>
2017-12-19 19:31:49 +0100

    Moved the checkbox popover script to popover.js (#129)

--- source/js/production/modules/checkboxWithPopover.js
+++
@@ -1,8 +0,0 @@
-  $('.a-switch').find('[data-toggle=popover]').on('change', function() {
-    if ($(this).is(':checked')) {
-      $(this).popover('hide');
-    } else {
-      $(this).popover('show');
-    }
-  });
-
