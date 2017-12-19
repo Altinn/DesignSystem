796854ef4c2b725d518117bc2807621ede332d83
Marianne Røsvik <post@mrosvik.no>
2017-12-19 07:19:58 +0100

    I had to link the modal demo to gh-pages to make it work from storefront (#122)

--- gulpfile.js
+++ gulpfile.js
@@ -475,7 +475,6 @@ gulp.task('copy:export-to-styleguide', function(done) {
     .pipe(replace('<body class=""', '<body class="a-bgWhite p-1"'))
     .pipe(replace('../../images/', '/designsystem-styleguide/images/'))
     .pipe(replace('..&#x2F;..&#x2F;images&#x2F;', '/designsystem-styleguide/images/'))
-    .pipe(replace('.markup-only.html', '.markup-only.rendered.html'))
     .pipe(gulp.dest('../designsystem-styleguide/_includes/patterns'));
 
     // Export public/patterns directory to style guide patterns directory
@@ -492,7 +491,6 @@ gulp.task('copy:export-to-styleguide', function(done) {
   .pipe(replace('<body class=""', '<body class="a-bgWhite p-1"'))
   .pipe(replace('../../images/', '/designsystem-styleguide/images/'))
   .pipe(replace('..&#x2F;..&#x2F;images&#x2F;', '/designsystem-styleguide/images/'))
-  .pipe(replace('.markup-only.html', '.markup-only.rendered.html'))
   .pipe(gulp.dest('../designsystem-styleguide/patterns'));
 
   // Export css directory to style guide css directory
