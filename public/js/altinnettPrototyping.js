c3ead18104f76c7ad7b15854364965798d6b769a
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2018-01-12 12:57:45 +0100

    latest public folder from master

--- public/js/altinnettPrototyping.js
+++ public/js/altinnettPrototyping.js
@@ -14,6 +14,55 @@ var fixPatternLinks = function() {
   }
 };
 
+/*
+Search datatable with highlight using external package mark.js
+Search field needs attribute data-search-algorithm="show-and-highlight"
+Searchable elements need attribute data-searchable="true"
+List elements that should be ignored during search need the class a-js-ignoreDuringSearch
+*/
+var mark = function() {
+  var $elements;
+  var input = $(this).val();
+  var options = {
+    // comment out to ignore html tags in searchable strings
+    // ie: string1 <tag>string2</tag>
+    separateWordSearch: false
+  };
+
+  $.each(this.dataset.searchTarget.split(','), function() {
+    // Reset visibility of all rows
+    var target = '#' + this.toString();
+
+    $(target + ' li:not(.a-js-ignoreDuringSearch):not(.a-list-header)').show();
+
+    $(target).find('*[data-searchable="true"]').unmark().mark(input, options);
+
+    // Hide unmarked rows
+    if (input.length > 0) {
+      $elements = $(target + ' li:not(.a-js-ignoreDuringSearch):not(.a-list-header)');
+      $elements.each(function() {
+        if ($(this).find('mark').length === 0) {
+          $(this).hide();
+        }
+      });
+    } else {
+      $elements = null;
+    }
+
+    if (!$elements || $elements.find('mark').length > 0) {
+      $(target + ' .a-js-noSearchResults').closest('li').hide();
+    } else {
+      $(target + ' .a-js-noSearchResults-phrase').text(input);
+      $(target + ' .a-js-noSearchResults').closest('li').show();
+    }
+  });
+};
+
+var initSearchWithHighlight = function() {
+  $('.a-js-noSearchResults').closest('li').hide();
+  $('input[data-search-algorithm="show-and-highlight"]').on('input', mark);
+};
+
 var setupTruncateLines = function() {
   setTimeout(function() {
     // Max two lines for all screen sizes
@@ -663,6 +712,255 @@ AltinnModal = {
   }
 };
 
+/* globals $ */
+var feedbackToggle = function() {
+  if ($('.a-js-feedbackToggle').length > 0) {
+    $('.a-js-feedbackToggle').closest('fieldset').next().hide();
+    $('.a-js-feedbackToggle').closest('fieldset').next().next()
+      .hide();
+    $('.a-js-feedbackToggle').closest('fieldset').next().next()
+      .next()
+      .hide();
+    $('.a-js-feedbackToggle').each(function() {
+      $(this).find('input[type=radio]').change(function() {
+        if ($(this).val() === 'radio1' && $(this).is(':checked')) {
+          $(this).closest('fieldset').next().show();
+          $(this).closest('fieldset').next().next()
+            .hide();
+          $(this).closest('fieldset').next().next()
+            .next()
+            .hide();
+        } else if ($(this).val() === 'radio2' && $(this).is(':checked')) {
+          $(this).closest('fieldset').next().hide();
+          $(this).closest('fieldset').next().next()
+            .show();
+          $(this).closest('fieldset').next().next()
+            .next()
+            .show();
+        }
+      });
+      $(this).closest('form').find('button').on('click', function() {
+        $(this).closest('fieldset').next().show();
+        $(this).closest('fieldset').next().next()
+          .hide();
+        $(this).closest('fieldset').next().next()
+          .next()
+          .hide();
+      }.bind(this));
+    });
+  }
+};
+
+/* globals compareTo */
+var sortListAlphanumerically = function(src, sortIndex) {
+  var $list = $(src).closest('.a-list-container').find('.a-list');
+  var rows = $list.find('li:not(.a-list-header)');
+  var reverse;
+
+  var active = $(src).hasClass('a-active');
+  if (!active) {
+    $(src).closest('.a-list-container').find('.a-list-sortHeader').removeClass('a-active')
+      .removeClass('a-js-reverse-sort');
+    $(src).addClass('a-active');
+  } else {
+    $(src).toggleClass('a-js-reverse-sort');
+  }
+
+  reverse = $(src).hasClass('a-js-reverse-sort');
+
+  rows.sort(function(a, b) {
+    var A = $($($($(a).children()[0]).children()[sortIndex]).find('.a-js-sortValue')[0]).text()
+      .toUpperCase();
+    var B = $($($($(b).children()[0]).children()[sortIndex]).find('.a-js-sortValue')[0]).text()
+      .toUpperCase();
+    return reverse ? compareTo(B, A) : compareTo(A, B);
+  });
+
+  $.each(rows, function(index, row) {
+    if ($(row).find('.a-js-sortValue').length > 0) {
+      $list.append(row);
+    }
+  });
+
+    // handles load more row
+  $.each(rows, function(index, row) {
+    if ($(row).find('.a-js-sortValue').length === 0) {
+      $list.append(row);
+    }
+  });
+};
+
+var defaultListSort = function() {
+  $('.a-list-container').each(function() {
+    var sortHeader = $(this).find('.a-list-sortHeader')[0];
+    var index = $(sortHeader).index();
+    sortListAlphanumerically(sortHeader, index);
+  });
+};
+
+var addListSortHandler = function() {
+  $('.a-list-sortHeader').on('click', function() {
+    var index = $(this).index();
+    sortListAlphanumerically(this, index);
+  });
+
+  defaultListSort();
+};
+
+var compareTo = function(firstItem, secondItem) {
+  var first;
+  var second;
+  if (firstItem === undefined && secondItem === undefined) {
+    return 0;
+  }
+  if (firstItem === undefined) {
+    return 1;
+  }
+  if (secondItem === undefined) {
+    return -1;
+  }
+
+  first = firstItem.toString();
+  second = secondItem.toString();
+  if (first < second) {
+    return -1;
+  }
+
+  if (first > second) {
+    return 1;
+  }
+  return 0;
+};
+
+/* globals $ */
+
+var newsArchive = function() {
+  var page = 1;
+  var numberOfItemsPerPage = 5;
+  var rootSelector = '.a-newsArchive';
+  var articleCssSelector = '.a-linkArticle';
+  var $loadMoreButton = $('.a-btn-loadMore');
+  var articlesCount = $(articleCssSelector).length;
+
+  function visibleItems() {
+    return page * numberOfItemsPerPage;
+  }
+
+  function setButtonVisibility() {
+    if (articlesCount <= visibleItems()) {
+      $loadMoreButton.hide();
+    }
+  }
+
+  if ($(rootSelector).length > 0) {
+    $loadMoreButton.on('click', function() {
+      var articles;
+      page += 1;
+      $(':nth-child(n+' + (visibleItems() + 1) + ')' + articleCssSelector).hide();
+      $(':nth-child(-n+' + visibleItems() + ')' + articleCssSelector).show();
+      setButtonVisibility();
+    });
+
+    setButtonVisibility();
+  }
+};
+
+/* globals mobileNavigation */
+var wasDark = $('header').hasClass('a-darkBackground');
+var action = function(e) {
+  if ($(e.target).closest('.a-globalNav-main').length === 0 &&
+  $(e.target).closest('.navbar-toggler').length === 0) {
+    if ($('.a-globalNav-main').is(':visible')) {
+      $('.navbar-toggler').attr('data-jsexpanded', 'false');
+      $('.a-globalNav-main').hide();
+      $('body').css('background-color', '');
+      if (wasDark) {
+        $('header').addClass('a-darkBackground');
+      }
+      $('.a-page').children(':not(header)').removeClass('a-js-hidden');
+    }
+  } else if ($(e.target).closest('.navbar-toggler').length > 0) {
+    if ($('.a-globalNav-main').is(':visible')) {
+      $('.navbar-toggler').attr('data-jsexpanded', 'false');
+      $('.a-globalNav-main').hide();
+      $('body').css('background-color', '');
+      if (wasDark) {
+        $('header').addClass('a-darkBackground');
+      }
+      $('.a-page').children(':not(header)').removeClass('a-js-hidden');
+    } else {
+      $('.navbar-toggler').attr('data-jsexpanded', 'true');
+      $('.a-globalNav-main').show();
+      $('body').css('background-color', '#000');
+      if (wasDark) {
+        $('header').removeClass('a-darkBackground');
+      }
+      $('.a-page').children(':not(header)').addClass('a-js-hidden');
+    }
+  }
+};
+function menuHandler() {
+  // enable tabbing and mouse click on mobile menu btn
+  if ($('body').width() < 768) {
+    $('body').on('click', action);
+  }
+}
+menuHandler();
+$(window).on('resize', function() {
+  $('body').off('click', action);
+  menuHandler();
+});
+
+/* globals $ */
+var subscribe = function() {
+  var validate = function(elem, skipVal) {
+    var re = new RegExp(elem.attr('data-val-regex-pattern'));
+    if (re.test(elem.val())) {
+      elem.closest('.a-card').find('button').removeAttr('disabled')
+        .removeClass('disabled');
+    } else {
+      elem.closest('.a-card').find('button').attr('disabled', 'disabled')
+        .addClass('disabled');
+    }
+  };
+  if ($('.a-js-subscribe').length > 0) {
+    $('.a-js-subscribe').each(function() {
+      var _this = $(this);
+      _this.closest('.a-card').find('.a-js-finishText').hide();
+      _this.closest('.a-card').find('.a-js-altText').hide();
+      _this.find('input').on('input', function() {
+        validate($(this));
+      });
+      _this.find('input').on('keypress', function(e) {
+        var keyCode = e.keyCode || e.which;
+        if (keyCode === 13) {
+          e.preventDefault();
+          if (!_this.find('button').hasClass('disabled')) {
+            _this.find('button').trigger('click');
+          }
+        }
+      });
+      _this.find('button').on('click', function() {
+        var __this = $(this);
+        _this.find('.a-form-group-items.input-group').addClass('disabled')
+          .addClass('a-input-approved');
+        _this.find('input').attr('disabled', 'disabled');
+        _this.closest('.a-card').find('.a-js-finishText').show();
+        _this.closest('.a-card').find('.a-js-altText').hide();
+        __this.hide();
+        $('.a-js-undo').on('click', function() {
+          _this.find('.a-form-group-items.input-group').removeClass('disabled')
+            .removeClass('a-input-approved');
+          _this.find('input').removeAttr('disabled');
+          _this.closest('.a-card').find('.a-js-finishText').hide();
+          _this.closest('.a-card').find('.a-js-altText').show();
+          __this.show();
+        });
+      });
+    });
+  }
+};
+
 /* globals
   setupExpandContent
 */
@@ -705,18 +1003,48 @@ $('body').on('hide.bs.collapse', '.a-collapsePanel-body', function() {
   }, 0);
 });
 
+/* globals $ */
+var questionnaireInteraction = function() {
+  $('.a-trigger-question').each(function() {
+    $(this).find('input').on('change', function() {
+      $(this).parent().parent().parent()
+        .parent()
+        .next()
+        .css('display', 'block');
+    });
+  });
+};
+
 /* globals
   fixPatternLinks,
+  initSearchWithHighlight,
   AltinnModal,
-  setupTruncateLines
+  addListSortHandler,
+  compareTo,
+  feedbackToggle,
+  newsArchive,
+  popoverGlobalInit,
+  popoverLocalInit,
+  setupTruncateLines,
+  subscribe,
+  questionnaireInteraction,
 */
 window.altinnettInit = function() {
   // Only for prototyping
   fixPatternLinks();
+  initSearchWithHighlight();
 
   // Should also be included in production (dist)
   AltinnModal.init();
+  addListSortHandler();
+  compareTo();
+  feedbackToggle();
+  newsArchive();
+  popoverGlobalInit();
+  popoverLocalInit();
   setupTruncateLines();
+  subscribe();
+  questionnaireInteraction();
 };
 
 window.altinnettInit();
