4b004aff4a2468c71b8f8d917db9e63069949300
acn-dgopa <d.gopalswamy@avanade.com>
2018-01-29 09:02:36 +0100

    Changed names, added javascript and modified json

--- source/js/production/modules/notificationaddress.js
+++ source/js/production/modules/notificationaddress.js
@@ -12,9 +12,22 @@ $('body').on('keyup', '#text-input-notification-epost', function(e) {
   }
 });
 
-$('body').on('click', '#link-sms-first', function() {
-  $('#link-sms-first').addClass('d-none');
+$('body').on('click', '#link-sms', function() {
+  $('#link-sms').addClass('d-none');
   $('#additionalContactInfo').removeClass('d-none');
+  $('#addmore-sms').removeClass('d-none');
 });
 
-// $('#additionalContactInfo').adde
+$('body').on('click', '#link-sms-addmore', function() {
+  var clone = $('#notificationInfoSMSTemplate').clone();
+  $('#addmore-sms').before(clone.html());
+});
+
+$('body').on('click', '#link-addmore-email', function() {
+  // var index = $('li').length;
+  // var clone = $('#phoneNumber-1').clone();
+  var clone = $('#notificationInfoEmailTemplate').clone();
+  // clone.html($(clone).html().replace(/\[#\]/g, '[' + index + ']'));
+  // clone.html($(clone).html().replace(/%/g, index));
+  $('#addmore-email').before(clone.html());
+});
