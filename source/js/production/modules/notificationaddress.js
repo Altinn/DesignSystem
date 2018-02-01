e99a81d3fbe58482f9fb8912cd672f209479f49f
acn-dgopa <d.gopalswamy@avanade.com>
2018-02-01 17:04:47 +0100

    Fixes for design review comments

--- source/js/production/modules/notificationaddress.js
+++ source/js/production/modules/notificationaddress.js
@@ -1,15 +1,52 @@
+function hideDeleteButton() {
+  var emailInputCount = $('div.a-email:visible').length;
+  var smsInputCount = $('div.a-sms:visible').length;
+  if (emailInputCount === 0 && smsInputCount === 1) {
+    $('div.a-sms').find('.a-delete-sms').addClass('d-none');
+  } else if (emailInputCount === 1 && smsInputCount === 0) {
+    $('div.a-email').find('.a-delete-email').addClass('d-none');
+  } else {
+    $('div.a-sms').find('.a-delete-sms').removeClass('d-none');
+    $('div.a-email').find('.a-delete-email').removeClass('d-none');
+  }
+}
+
+function addContact(contactType, index, inputCount) {
+  var inputTemplateId;
+  var contactId;
+  var newContactId;
+  var templateId;
+  var deleteId;
+  contactId = '#' + contactType + 'Input-' + inputCount;
+  newContactId = contactType + 'Input-' + index;
+  templateId = '#' + contactType + 'Input-1';
+  deleteId = contactType + 'Delete-' + index;
+  if (inputCount === 0) {
+    $(templateId).removeClass('d-none');
+  } else if (inputCount === 1) {
+    inputTemplateId = '#' + $('div.a-' + contactType + ':visible').attr('id');
+    $(inputTemplateId).clone().attr('id', newContactId).insertAfter(inputTemplateId);
+  } else {
+    $(contactId).clone().attr('id', newContactId).insertAfter(contactId);
+  }
+  if (inputCount > 0) {
+    $('#' + newContactId).find('.a-delete').attr('id', deleteId);
+    $('#' + newContactId).find('input[type=email]').attr('value', '');
+    $('#' + newContactId).find('.a-input-phonenumber').attr('value', '');
+  }
+}
 
 $(document).ready(function() {
-  var smsCount = $('div.a-phone').length;
-  var emailCount = $('div.a-email').length;
-
-  /* if (smsCount === 1) {
-    $('#smsInput-1').find('.a-delete-phone').addClass('d-none');
-  } */
-  if (emailCount === 1) {
-    $('#emailInput-1').find('.a-delete-email').addClass('d-none');
-  }
+  hideDeleteButton();
 });
+
+function removeContact(contactType, index) {
+  if (index === '1') {
+    $('#' + contactType + 'Input-' + index).addClass('d-none');
+  } else {
+    $('#' + contactType + 'Input-' + index).remove();
+  }
+}
 
 $('body').on('keyup', '#text-input-notification-epost', function(e) {
   var epostLength = $('#text-input-notification-epost').val().length;
@@ -28,62 +65,39 @@ $('body').on('click', '#link-sms', function() {
 });
 
 $('body').on('click', '#link-sms-addmore', function() {
-  var inputCount = $('div.a-phone:visible').length;
+  var inputCount = $('div.a-sms:visible').length;
   var index = inputCount + 1;
-  var inputTemplateId;
 
-  if (inputCount === 0) {
-    $('#smsInput-1').removeClass('d-none');
-  } else if (inputCount === 1) {
-    inputTemplateId = '#' + $('div.a-phone').attr('id');
-    $(inputTemplateId).clone().attr('id', 'smsInput-' + index).insertAfter(inputTemplateId);
-  } else {
-    $('#smsInput-' + inputCount).clone().attr('id', 'smsInput-' + index).insertAfter('#smsInput-' + inputCount);
-  }
-  $('#smsInput-' + index).find('.a-delete').attr('id', 'smsDelete-' + index);
-  $('div.a-phone').find('.a-delete-phone').removeClass('d-none');
+  addContact('sms', index, inputCount);
+  hideDeleteButton();
 });
 
 $('body').on('click', '#link-addmore-email', function() {
-  var inputCount = $('div.a-email').length;
+  var inputCount = $('div.a-email:visible').length;
   var index = inputCount + 1;
-  var inputTemplateId;
-  if (inputCount === 1) {
-    inputTemplateId = '#' + $('div.a-email').attr('id');
-    $(inputTemplateId).clone().attr('id', 'emailInput-' + index).insertAfter(inputTemplateId);
-  } else {
-    $('#emailInput-' + inputCount).clone().attr('id', 'emailInput-' + index).insertAfter('#emailInput-' + inputCount);
-  }
-  $('div.a-email').find('.a-delete-email').removeClass('d-none');
-  $('#emailInput-' + index).find('.a-delete').attr('id', 'emailDelete-' + index);
+
+  addContact('email', index, inputCount);
+  hideDeleteButton();
 });
 
-$('body').on('click', 'div.a-phone .a-delete', function(e) {
+$('body').on('click', 'div.a-sms .a-delete', function(e) {
   var index = (e.target.id).split('-')[1];
   var inputCount = 0;
 
-  if (index === '1') {
-    $('#smsInput-' + index).addClass('d-none');
-  } else {
-    $('#smsInput-' + index).remove();
+  removeContact('sms', index);
+  inputCount = $('div.a-sms:visible').length;
+  if (inputCount === 0) {
+    $('#phoneTitle').addClass('d-none');
+    $('#addmore-sms').addClass('d-none');
+    $('#link-sms').removeClass('d-none');
   }
-
-    /* inputCount = $('div.a-phone').length;
-    if (inputCount === 1) {
-    $('div.a-phone').find('.a-delete-phone').addClass('d-none');
-  } */
+  hideDeleteButton();
 });
 
 $('body').on('click', 'div.a-email  .a-delete', function(e) {
   var index = (e.target.id).split('-')[1];
-  var inputCount = 0;
-  if (index === 1) {
-    $('#emailInput-' + index).addClass('d-none');
-  } else {
-    $('#emailInput-' + index).remove();
-  }
-  inputCount = $('div.a-email').length;
-  if (inputCount === 1) {
-    $('div.a-email').find('.a-delete-email').addClass('d-none');
-  }
+
+  removeContact('email', index);
+  hideDeleteButton();
 });
+
