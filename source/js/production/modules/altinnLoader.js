29f4cf4bcb48390d8ad3c7fd24fb3258c5ca4c86
Truls Clark <truls.clark@gmail.com>
2018-04-25 08:49:40 +0200

    added logic to disable modalbuttons while loading content

--- source/js/production/modules/altinnLoader.js
+++ source/js/production/modules/altinnLoader.js
@@ -5,9 +5,13 @@ AltinnLoader = {
     if ($target.find('.loader-container').length === 0) {
       $target.prepend('<div class="loader-container"><div class="loader loader-ellipsis"></div></div>');
     }
+    $('button.a-modal-back').attr('disabled', true);
+    $('button.a-modal-close').attr('disabled', true);
   },
 
   removeLoader: function($target) {
     $target.find('.loader-container').remove();
+    $('button.a-modal-back').attr('disabled', false);
+    $('button.a-modal-close').attr('disabled', false);
   }
 };
