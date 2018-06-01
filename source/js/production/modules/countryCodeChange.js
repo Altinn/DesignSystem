070940ba47b25c2428d279a91ee10525198b1e76
Mats Gimming Myhre <mats.g.myhre@gmail.com>
2018-06-01 12:34:06 +0200

    Refactor: Logically expanding max-width based on length of country code.

--- source/js/production/modules/countryCodeChange.js
+++ source/js/production/modules/countryCodeChange.js
@@ -1,18 +1,14 @@
 var onCountryCodeChange = function() {
+  var initialWidth = 55;
   $('.a-js-countryCode').keyup(function() {
     var inputVal = $(this).val();
-    switch (inputVal.length) {
-    case 4:
-      $(this).css('max-width', '62px');
-      break;
-    case 5:
-      $(this).css('max-width', '71px');
-      break;
-    case 6:
-      $(this).css('max-width', '80px');
-      break;
-    default:
-      $(this).css('max-width', '55px');
+    var newWidth;
+
+    if (inputVal.length > 3) {
+      newWidth = initialWidth + ((inputVal.length - 3) * 9);
+      $(this).css('max-width', newWidth + 'px');
+    } else {
+      $(this).css('max-width', '');
     }
   });
 };
