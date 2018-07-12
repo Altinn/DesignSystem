d465459fffc7190a00a75ae3fab7ee0fb99e9ec8
Mats Gimming Myhre <mats.g.myhre@gmail.com>
2018-07-12 12:35:40 +0200

    Fixed coding error in altinnDropdown.js. Safari users was unable to select from dropdown.

--- source/js/production/modules/altinnDropdown.js
+++ source/js/production/modules/altinnDropdown.js
@@ -4,7 +4,7 @@ AltinnDropdown = {
   init: function() {
     var that = this;
     $('body').on('click', '[data-toggle="altinn-dropdown"] .a-dropdown-item', function() {
-      var $dropdownElement = $(this).closest('[data-toggle="altinn-dropdown"');
+      var $dropdownElement = $(this).closest('[data-toggle="altinn-dropdown"]');
       if ($(this).data('value')) {
         $dropdownElement.find('.a-js-altinnDropdown-value').val($(this).data('value'));
       }
