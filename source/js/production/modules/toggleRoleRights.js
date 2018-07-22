3477250b8f1fb38c287dd0d851dffed518649077
Mats Gimming Myhre <mats.g.myhre@gmail.com>
2018-07-22 16:22:53 +0200

    Added accessability tabbing.

--- source/js/production/modules/toggleRoleRights.js
+++ source/js/production/modules/toggleRoleRights.js
@@ -1,11 +1,43 @@
 var toggleRoleRightsInit = function() {
   // When clicked on les, skrive or signer icon, toggle the disable on/off.
   $('.a-js-toggleRights').on('click', function() {
-    $(this).toggleClass('a-iconStrikeThrough a-disabledIcon');
+    console.log('toggle', $(this).children(':first'));
+    $(this).children(':first').toggleClass('a-iconStrikeThrough a-disabledIcon');
   });
 
   // When clicking on the delete or angre icon, toggle the deleted row on/off.
   $('.a-js-toggleRightsDelete').on('click', function() {
     $(this).closest('li').toggleClass('a-deleted a-selected a-defaultCursor');
+
+    // if closest li has class a-deleted.
+    if ($(this).closest('li').hasClass('a-deleted')) {
+      // remove tabindex of rights buttons
+      $(this)
+        .parent()
+        .prev()
+        .children()
+        .prop('tabindex', '-1');
+
+      // set tabindex -1 on ai-del
+      $(this).prop('tabindex', '-1');
+
+      // set tabdindex and focus to ai-undo
+      $(this).next().prop('tabindex', '0');
+      $(this).next().focus();
+    // else if closest li does not have a-deleted
+    } else {
+      $(this)
+      .parent()
+      .prev()
+      .children()
+      .prop('tabindex', '0');
+
+      // set tabindex -1 on ai-undo
+      $(this).prop('tabindex', '-1');
+
+      // set focus to ai-del
+      $(this).prev().prop('tabindex', '0');
+      $(this).prev().focus();
+    }
   });
 };
