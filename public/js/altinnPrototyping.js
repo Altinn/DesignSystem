48577b20eb02cddb6ab5fb89b0538ebaf0b35f56
BOUVET\janicke.falch <janicke.falch@bouvet.no>
2018-07-31 08:39:49 +0200

    Latest public from master

--- public/js/altinnPrototyping.js
+++ public/js/altinnPrototyping.js
@@ -1024,18 +1024,23 @@ var addListExpandHandler = function() {
   });
 };
 
+/* eslint vars-on-top: 0 */
 /* globals AltinnDropdown */
 /* globals AltinnDropdown:true */
 AltinnDropdown = {
   init: function() {
     var that = this;
     $('body').on('click', '[data-toggle="altinn-dropdown"] .a-dropdown-item', function() {
-      var $dropdownElement = $(this).closest('[data-toggle="altinn-dropdown"');
+      var $dropdownElement = $(this).closest('[data-toggle="altinn-dropdown"]');
       if ($(this).data('value')) {
         $dropdownElement.find('.a-js-altinnDropdown-value').val($(this).data('value'));
       }
 
       $dropdownElement.find('.a-dropdown-toggle').html($(this).html());
+
+      // Focus the dropdownmenu element after click on item in dropdownmenu
+      var id = $(this).closest('.a-dropdown-menu').attr('aria-labelledby');
+      $('#' + id).focus();
     });
   }
 };
