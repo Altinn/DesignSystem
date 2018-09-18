44f18eed9f727544c09329759bd8f60597f9370c
Catriona MacKenzie <catriona.mackenzie@bouvet.no>
2018-09-18 19:27:51 +0200

    [ALTDIG-149] Rettet lint feil

--- source/js/production/modules/slick-carousel.js
+++ source/js/production/modules/slick-carousel.js
@@ -4,8 +4,8 @@ var setupSlickCarousel = function() {
       $('[data-slideshow]').slick({
         infinite: false,
         appendArrows: $('[data-slideshow-buttons]'),
-        prevArrow: "<button type='button' class='slick-prev an-slideshow__button an-slideshow__button--previous'><i class='ai ai-arrowright'><span class='sr-only'>Forrige</span></i></button>",
-        nextArrow: "<button type='button' class='slick-prev an-slideshow__button an-slideshow__button--next'><i class='ai ai-arrowright'><span class='sr-only'>Neste</span></i></button>"
+        prevArrow: '<button type="button" class="slick-prev an-slideshow__button an-slideshow__button--previous"><i class="ai ai-arrowright"><span class="sr-only">Forrige</span></i></button>',
+        nextArrow: '<button type="button" class="slick-prev an-slideshow__button an-slideshow__button--next"><i class="ai ai-arrowright"><span class="sr-only">Neste</span></i></button>'
       });
     });
   }
