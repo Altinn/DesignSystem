b08f50a8475a2d875ffc9e89e42891cd890da172
Catriona MacKenzie <catriona.mackenzie@bouvet.no>
2018-09-19 13:19:12 +0200

    [ALTDIG-149] Mobil tilpasninger for slideshow

--- source/js/production/modules/slick-carousel.js
+++ source/js/production/modules/slick-carousel.js
@@ -1,11 +1,13 @@
 var setupSlickCarousel = function() {
   if ($('[data-slideshow]').length) {
     $(document).ready(function() {
-      $('[data-slideshow]').slick({
-        infinite: false,
-        appendArrows: $('[data-slideshow-buttons]'),
-        prevArrow: '<button type="button" class="slick-prev an-slideshow__button an-slideshow__button--previous"><i class="ai ai-arrowright"><span class="sr-only">Forrige</span></i></button>',
-        nextArrow: '<button type="button" class="slick-prev an-slideshow__button an-slideshow__button--next"><i class="ai ai-arrowright"><span class="sr-only">Neste</span></i></button>'
+      $('[data-slideshow]').each(function() {
+        $(this).slick({
+          infinite: false,
+          appendArrows: $(this).next('[data-slideshow-buttons]'),
+          prevArrow: '<button type="button" class="slick-prev an-slideshow-button an-slideshow-button-previous"><i class="ai ai-arrowright"><span class="sr-only">Forrige</span></i></button>',
+          nextArrow: '<button type="button" class="slick-prev an-slideshow-button an-slideshow-button-next"><i class="ai ai-arrowright"><span class="sr-only">Neste</span></i></button>'
+        });
       });
     });
   }
