31d524dfb6bb4376722a1671eca4aef348bef6d8
jonkjetiloye <jon.kjetil.oye@avanade.com>
2019-02-20 09:37:52 +0100

    - Oppdatert resultat liste for tjenestesøk til å inneholde ServiceOwner under "Andre med rettigheter" og "Varsel for enkelt tjenester" - Oppdatert oppretting av en ny lokal rolle til samme nye design som "Lokal rolle rediger"

--- source/js/prototyping/modules/searchWithAutocomplete.js
+++ source/js/prototyping/modules/searchWithAutocomplete.js
@@ -1,11 +1,11 @@
 // Hard-coded data, should be replaced with JSON
 var availableTags = [
-  { label: '1. ACC Security level 2 MAG' },
-  { label: '2. Corres test 250116' },
-  { label: '3. PSA Skatteoppgjør personlig' },
-  { label: '4. RF-1400 Melding om flytting innenlands' },
-  { label: '5. Aksjeoppgaven 2014' },
-  { label: '6. Et veldig langt punkt i lista som bør gå over alle bredder og grenser, men samtidig oppføre seg riktig i layout. Se så lang tekst dette her er.' }
+  { label: 'ACC Security level 2 MAG, Accenture Test', service: 'ACC Security level 2 MAG', serviceOwner: 'Accenture Test' },
+  { label: 'Corres test 250116, Accenture Test', service: 'Corres test 250116', serviceOwner: 'Accenture Test' },
+  { label: 'PSA Skatteoppgjør personlig, Skatteetaten', service: 'PSA Skatteoppgjør personlig', serviceOwner: 'Skatteetaten' },
+  { label: 'RF-1400 Melding om flytting innenlands,Skatteetaten', service: 'RF-1400 Melding om flytting innenlands', serviceOwner: 'Skatteetaten' },
+  { label: 'Aksjeoppgaven 2014, Skatteetaten', service: 'Aksjeoppgaven 2014', serviceOwner: 'Skatteetaten' },
+  { label: 'Et veldig langt punkt i lista som bør gå over alle bredder og grenser, men samtidig oppføre seg riktig i layout. Se så lang tekst dette her er., accenture', service: 'Et veldig langt punkt i lista som bør gå over alle bredder og grenser, men samtidig oppføre seg riktig i layout. Se så lang tekst dette her er.', serviceOwner: 'accenture' }
 ];
 
 // Hard-coded texts, should be replaced with custom strings
@@ -26,10 +26,25 @@ var searchWithAutocomplete = function() {
       var iLength = items.length;
 
       $.each(items, function(index, item) {
+        // build  item in the list
+        var innerHtmlForItem = '<div class="row">' +
+          '<div class="col-sm-7 col-md-5 col-lg-6 pl-md-2 pl-lg-2 pr-2" data-searchable="true">' +
+            '<span class="a-js-sortValue a-list-longtext" title="ACC Security level 2 MAG">' + item.service + '</span>' +
+          '</div>' +
+          '<div class="d-none d-md-block col-md-3 col-lg-4 pl-md-2 pl-lg-1 pr-2" data-searchable="true">' +
+            '<span class="a-js-sortValue a-list-longtext" title="Testetat for Accenture">' + item.serviceOwner + '</span>' +
+          '</div>' +
+        '</div>';
+
         var li = that._renderItemData(ul, item);
-        li.attr('role', 'menu');
-        li.addClass('a-dotted');
+        li.data('item.autocomplete', item);
+        li.children().first().remove();
+        li.append(innerHtmlForItem);
         li.children().first().attr('role', 'button');
+        li.attr('role', 'menu');
+        li.addClass('a-dotted a-selectable');
+        li.attr('id', 'menu-item-' + index);
+        li.attr('onclick', "location.href='/patterns/04-sider-portal-92-andre-med-rettigheter-00-andre-med-rettigheter-tildel-enkeltrettigheter/04-sider-portal-92-andre-med-rettigheter-00-andre-med-rettigheter-tildel-enkeltrettigheter.html'")
       });
 
       if (iLength === availableTags.length) {
@@ -84,8 +99,6 @@ var searchWithAutocomplete = function() {
   }).bind('click', function(e) { // TODO should also open on tab focus? issue 3766
     if ($(this).catcomplete('widget').is(':visible')) {
       $(this).catcomplete('close');
-    } else {
-      $(this).catcomplete('search', $(this).val());
     }
   });
 };
--- source/js/prototyping/modules/searchWithAutocompleteVarselEnkeltTjeneste.js
+++ source/js/prototyping/modules/searchWithAutocompleteVarselEnkeltTjeneste.js
@@ -1,5 +1,3 @@
-/* eslint vars-on-top: 1 */
-
 // Hard-coded data, should be replaced with JSON
 var availableTags = [
   { label: 'ACC Security level 2 MAG, Accenture Test', service: 'ACC Security level 2 MAG', serviceOwner: 'Accenture Test' },
@@ -16,7 +14,7 @@ var numberOfResultsLabel = ' treff. Bruk pil opp og pil ned for å navigere i re
 var noResultsLabel = 'Ingen treff';
 var moreThanMaxLabel = 'Listen viser kun de første 100 treff. Vennligst begrens søket ditt';
 
-var searchWithMultipleSelectInAutoComplete = function() {
+var searchWithAutocompleteVarsel = function() {
   $.widget('custom.catcomplete', $.ui.autocomplete, ({
     _create: function() {
       this._super();
@@ -36,10 +34,6 @@ var searchWithMultipleSelectInAutoComplete = function() {
           '<div class="d-none d-md-block col-md-3 col-lg-4 pl-md-2 pl-lg-1 pr-2" data-searchable="true">' +
             '<span class="a-js-sortValue a-list-longtext" title="Testetat for Accenture">' + item.serviceOwner + '</span>' +
           '</div>' +
-          '<div class="text-right col-sm-5 col-md-4 col-lg-2 pl-md-2 pl-lg-1 pr-sm-0 pr-md-2">' +
-              '<span class="a-fontBold a-btn-icon-text a-hiddenWhenSelected ">+Legg til</span>' +
-              '<span class="a-fontBold d-none d-sm-block a-visibleWhenSelected">Lagt til</span>' +
-          '</div>' +
         '</div>';
 
         var li = that._renderItemData(ul, item);
@@ -50,11 +44,6 @@ var searchWithMultipleSelectInAutoComplete = function() {
         li.attr('role', 'menu');
         li.addClass('a-dotted a-selectable');
         li.attr('id', 'menu-item-' + index);
-        /* var li = that._renderItemData(ul, item);
-        console.log(li);
-        li.attr('role', 'menu');
-        li.addClass('a-dotted');
-        li.children().first().attr('role', 'button');*/
       });
 
       if (iLength === availableTags.length) {
@@ -68,46 +57,19 @@ var searchWithMultipleSelectInAutoComplete = function() {
       if (iLength >= 3) {
         ul.append('<li class=\'a-js-autocomplete-header a-dotted a-info\'>' + moreThanMaxLabel + '</li>');
       }
-
-      // Lukkeknapp
-      /*
-      ul.append('<li class="a-js-autocomplete-header ml-2 a-no-border-bottom">' +
-                  '<div class="mt-1 mb-1 d-flex justify-content-center">' +
-                    '<button id="closeAutoCompleteMenu" class="a-btn pl-1 pr-1">Lukk</button>' +
-                  '</div>' +
-                '</li>');
-      */
     },
-    oldClose: $.ui.autocomplete.prototype.close,
-    close: function(event) {
-      var bolClose;
-      if (event) {
-        bolClose = false;
-      } else {
-        bolClose = true;
-        $('button:contains(\'Lagre\')').removeAttr('disabled');
-      }
-
-      // Actuall close
-      if (bolClose) {
-        this.oldClose(event);
-      }
-
-      return false;
-    }
   }));
 
-  $('.a-js-autocomplete-multiplecolumns').catcomplete({
+  $('.a-js-autocomplete-varsel').catcomplete({
     // delay: 200, // set appropriate delay for ajax call
     source: availableTags,
     appendTo: '.a-autocomplete-container',
     minLength: 0,
     classes: {
-      'ui-autocomplete': 'a-list a-multipleSelectInAutoComplete',
+      'ui-autocomplete': 'a-list',
       'ui-menu-item': 'a-dotted'
     },
     open: function(event, ui) {
-      $('button:contains(\'Lagre\')').attr('disabled', 'disabled');
       $('.ui-autocomplete').removeAttr('style'); // remove inline positioning and display of amount results
       $('.ui-autocomplete .ui-menu-item').not(':first-of-type').addClass('a-clickable');
     },
@@ -123,7 +85,6 @@ var searchWithMultipleSelectInAutoComplete = function() {
     },
     response: function(event, ui) {
       var el;
-      // console.log(ui.content.length);
       if (ui.content.length === 0) {
         el = {
           isNoResultsLabel: true,
@@ -150,7 +111,7 @@ var searchWithMultipleSelectInAutoComplete = function() {
 
       // ONLY FOR DESIGNSYSTEM PROTOTYPING
       // eslint-disable-next-line
-      console.log('Prototyping feature needs to be commented out in searchWithMultipleSelectInAutoComplete.js');
+      console.log('Prototyping feature needs to be commented out in searchWithAutocompleteVarselEnkeltTjeneste.js');
       // Add the clicked rights to list, only if if not already in list
       if ($('.a-list-container').find('div:contains(' + ui.item.service + ')').length === 0) {
         // eslint-disable-next-line
