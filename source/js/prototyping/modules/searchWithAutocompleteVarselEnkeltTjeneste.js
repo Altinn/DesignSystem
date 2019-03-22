4bd161ea24e2ccf742f1f5ffc41d6c49dd4acc56
Magnus Nermark <magnusnermark@gmail.com>
2019-03-22 07:34:48 +0100

    minor changes

--- source/js/prototyping/modules/searchWithAutocompleteVarselEnkeltTjeneste.js
+++ source/js/prototyping/modules/searchWithAutocompleteVarselEnkeltTjeneste.js
@@ -36,7 +36,7 @@ var searchWithAutocompleteVarsel = function() {
           '</div>' +
           '<div class="text-right col-sm-5 col-md-4 col-lg-2 pl-md-2 pl-lg-1 pr-sm-0 pr-md-2">' +
               '<span class="a-fontBold a-btn-icon-text a-hiddenWhenSelected ">+Legg til</span>' +
-              '<span class="a-fontBold d-none d-sm-block a-visibleWhenSelected">Lagt til</span>' +
+              '<span class="a-fontBold d-sm-block a-visibleWhenSelected">Lagt til</span>' +
           '</div>' +
         '</div>';
 
@@ -143,3 +143,10 @@ var searchWithAutocompleteVarsel = function() {
     }
   });
 };
+
+// When pressing "ESC"
+$(document).on('keyup', function(evt) {
+  if (evt.keyCode === 27) {
+      $('.a-multipleSelectInAutoComplete').attr('style', 'display:none');
+  }
+});
