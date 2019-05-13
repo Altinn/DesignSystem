c18db6ed0df67dfe1419dd23e05796b21d5c0bad
jonkjetiloye <jon.kjetil.oye@avanade.com>
2019-05-13 14:38:29 +0200

    Oppdatering for HADM varsling med popover funksjonalitet

--- source/js/prototyping/modules/toggleCriticalRoleSelected.js
+++ source/js/prototyping/modules/toggleCriticalRoleSelected.js
@@ -1,27 +1,32 @@
-$('.a-js-selectable-checkbox').on('change', function(e) { 
+$('.a-js-selectable-checkbox').on('change', function(e) {
+  var roleid; // Id for rollen som ble endret
+  var isSelected; // Hvor vidt rollen ble valgt eller fjernet
+  var $warn; // Selector for advarselstekst
+  var $contnorm; // Container for vanlig "Ferdig"-knapp
+  var $contcrit; // Container for rød "Jeg forstår"-knapp
 
   // Class på parent <li> som indikerer at denne er flagget som kritisk
-  if (!$(e.target).parents('li.a-js-critical-role-selector').length)
-      return;   
+  if (!$(e.target).parents('li.a-js-critical-role-selector').length) {
+    return;
+  }
 
-  var roleid = $(e.target).parents('li.a-js-critical-role-selector')[0].id;
-  var isSelected = $(e.target).is(':checked');
-  var $warn = $('.a-js-critical-role-msg-' + roleid); // Selector for advarselstekst
-  var $contnorm = $('.a-js-standard-role-container'); // Container for vanlig "Ferdig"-knapp
-  var $contcrit = $('.a-js-critical-role-container'); // Container for rød "Jeg forstår"-knapp
+  roleid = $(e.target).parents('li.a-js-critical-role-selector')[0].id;
+  isSelected = $(e.target).is(':checked');
+  $warn = $('.a-js-critical-role-msg-' + roleid);
+  $contnorm = $('.a-js-standard-role-container');
+  $contcrit = $('.a-js-critical-role-container');
 
   if (isSelected) {
-      $warn.show();
-      $contcrit.show();
-      $contnorm.hide();
-  }
-  else {
-      $warn.hide();
+    $warn.show();
+    $contcrit.show();
+    $contnorm.hide();
+  } else {
+    $warn.hide();
   }
 
   // Sjekk om det er igjen noen kritiske rolle-advarsler
   if (!$('.a-js-critical-role-msg').filter(':visible').length) {
-      $contcrit.hide();
-      $contnorm.show();
+    $contcrit.hide();
+    $contnorm.show();
   }
 });
