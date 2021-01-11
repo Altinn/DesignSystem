3c050aa7c9646220f3b9dce6a9e907282a8aaad0
Marianne Røsvik <suparh@gmail.com>
2021-01-11 11:09:48 +0100

    mrosvik/altinndigital/timeline-component (#399)
    * added new timeline-compontent for articles with css and js. Also a new mediabox and a piechart component.
    
    * piechart fix
    
    * added guidelines for piechart

---
+++ source/_patterns/01-molekyler/03-media/40-kakediagram-med-tekst.md
@@ -0,0 +1,5 @@
+---
+state: indesignreview altinnett
+version: 0
+---
+Viktig: Det skal alltid oppgis tall/prosentandel i tekstformat i tillegg til det visuelle diagrammet. 
---
+++ source/js/production/modules/piechart.js
@@ -0,0 +1,28 @@
+// Pie chart
+var piechart = function() {
+  function $(selector, context, elements) {
+    context = context || document;
+    elements = context.querySelectorAll(selector);
+    return Array.prototype.slice.call(elements);
+  }
+
+  $('.a-pie').forEach(function(pie) {
+    var p = parseFloat(pie.textContent);
+    var NS = 'http://www.w3.org/2000/svg';
+    var svg = document.createElementNS(NS, 'svg');
+    var circle = document.createElementNS(NS, 'circle');
+    var title = document.createElementNS(NS, 'title');
+
+    circle.setAttribute('r', 16);
+    circle.setAttribute('cx', 16);
+    circle.setAttribute('cy', 16);
+    circle.setAttribute('stroke-dasharray', p + ' 100');
+
+    svg.setAttribute('viewBox', '0 0 32 32');
+    title.textContent = pie.textContent;
+    pie.textContent = '';
+    svg.appendChild(title);
+    svg.appendChild(circle);
+    pie.appendChild(svg);
+  });
+};
